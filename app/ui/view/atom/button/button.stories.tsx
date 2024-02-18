848c73fd66b1a7f2053aba545aac279ce9a7eed7
89882 <pgh123166@naver.com>
2024-02-18 19:59:51 +0900

    chore: Button stories import error 수정

--- app/ui/view/atom/button/button.stories.tsx
+++ app/ui/view/atom/button/button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from './button';
+import Button from './button';
 
 const meta = {
   title: 'ui/view/atom/Button',
