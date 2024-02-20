8326c320fc26ddd32a7b6c08f7d07aea8feb6754
SeonghunYang <didtjdgns852@naver.com>
2024-02-20 13:32:38 +0000

    refactor: 팔레트에 맞춰서 color 변경

--- app/utils/style/color.util.ts
+++ app/utils/style/color.util.ts
@@ -2,10 +2,10 @@ import { twMerge } from 'tailwind-merge';
 
 export const getInputColors = (isDisabled: boolean, hasError = false) => {
   return twMerge(
-    isDisabled ? 'bg-gray-100' : 'bg-white',
+    isDisabled ? 'bg-gray-1' : 'bg-white',
     !isDisabled && 'hover:bg-gray-50',
-    isDisabled && 'bg-gray-100',
-    hasError && 'text-red-500',
-    hasError ? 'border-red-500' : 'border-gray-200',
+    isDisabled && 'bg-gray-1',
+    hasError && 'text-red',
+    hasError ? 'border-red' : 'border-gray-2',
   );
 };
