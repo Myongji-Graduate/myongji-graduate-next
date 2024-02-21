d3b9e5bcab5ce70df25324c222b4c4ba74444623
yougyung <youkyeongs@naver.com>
2024-02-21 14:56:08 +0900

    style: navigation-bar position 수정

--- app/ui/view/molecule/navigation-bar.tsx
+++ app/ui/view/molecule/navigation-bar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function NavigationBar() {
   return (
-    <div className="fixed p-4 border-b-[1px] w-full">
+    <div className="absolute p-4 border-b-[1px] w-full">
       <Image
         className="md:h-10 h-7 w-[110px] md:w-[150px]"
         width={150}
