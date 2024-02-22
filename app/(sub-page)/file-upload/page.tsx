8d96cc8aa28d5c20649b9dac0ed336f7940f4b58
yougyung <youkyeongs@naver.com>
2024-02-22 12:55:30 +0900

    fix: 네이밍 수정

---
+++ app/(sub-page)/file-upload/page.tsx
@@ -0,0 +1,10 @@
+import ContentContainer from '../../ui/view/atom/content-container';
+
+export default function Page() {
+  return (
+    <ContentContainer style="flex flex-col items-center gap-8">
+      <div>요소1</div>
+      <div>요소2</div>
+    </ContentContainer>
+  );
+}
--- app/ui/view/atom/content-container.tsx
+++ app/ui/view/atom/content-container.tsx
@@ -1,18 +1,18 @@
 import { twMerge } from 'tailwind-merge';
 
-interface TemplateProps {
+interface ContentContainerProps {
   children: React.ReactNode;
   size?: 'md' | 'lg';
-  className?: string;
+  style?: string;
 }
 
-export default function Template({ children, size = 'md', className }: TemplateProps) {
+export default function ContentContainer({ children, size = 'md', style }: ContentContainerProps) {
   return (
     <div
       className={twMerge(
         'absolute bg-white top-[7rem] z-10 rounded-xl p-[1.5rem] shadow-lg max-md:w-full',
         size === 'md' ? 'md:w-[70%]' : 'md:w-[80%]',
-        className,
+        style,
       )}
     >
       {children}
