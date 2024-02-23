e9d20d67b8510fdcdbf7d3cebb769a7422a21845
yougyung <youkyeongs@naver.com>
2024-02-23 13:50:57 +0900

    fix: 속성명 수정

--- app/(sub-page)/file-upload/page.tsx
+++ app/(sub-page)/file-upload/page.tsx
@@ -2,7 +2,7 @@ import ContentContainer from '../../ui/view/atom/content-container';
 
 export default function Page() {
   return (
-    <ContentContainer style="flex flex-col items-center gap-8">
+    <ContentContainer className="flex flex-col items-center gap-8">
       <div>요소1</div>
       <div>요소2</div>
     </ContentContainer>
--- app/ui/view/atom/content-container.tsx
+++ app/ui/view/atom/content-container.tsx
@@ -3,16 +3,16 @@ import { twMerge } from 'tailwind-merge';
 interface ContentContainerProps {
   children: React.ReactNode;
   size?: 'md' | 'lg';
-  style?: string;
+  className?: string;
 }
 
-export default function ContentContainer({ children, size = 'md', style }: ContentContainerProps) {
+export default function ContentContainer({ children, size = 'md', className }: ContentContainerProps) {
   return (
     <div
       className={twMerge(
         'absolute bg-white top-[7rem] zIndex-1 rounded-xl p-[1.5rem] shadow-lg max-md:w-full',
         size === 'md' ? 'md:w-[70%]' : 'md:w-[80%]',
-        style,
+        className,
       )}
     >
       {children}
