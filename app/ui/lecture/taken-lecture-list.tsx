ca6c51d1e2bafd775ac409f6d2dd27e5a0866ddb
gahyuun <pgh123166@naver.com>
2024-02-28 02:05:09 +0900

    rename: taken lecture title -> taken lecture label

--- app/ui/lecture/taken-lecture-list.tsx
+++ app/ui/lecture/taken-lecture-list.tsx
@@ -1,6 +1,6 @@
 import { fetchTakenLectures } from '@/app/business/lecture/taken-lecture-list.query';
 import { Table } from '../view/molecule/table';
-import TakenLectureTitle from './taken-lecture-title';
+import TakenLectureLabel from './taken-lecture-label';
 
 const headerInfo = ['수강년도', '수강학기', '과목코드', '과목명', '학점'];
 
@@ -8,7 +8,7 @@ export default async function TakenLectureList() {
   const data = await fetchTakenLectures();
   return (
     <div className="w-[800px] flex flex-col gap-2">
-      <TakenLectureTitle />
+      <TakenLectureLabel />
       <Table headerInfo={headerInfo} data={data.takenLectures} />
     </div>
   );
