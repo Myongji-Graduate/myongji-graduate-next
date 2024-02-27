9cc22819e24aa3bc93c7054cdda21df1930f5794
SeonghunYang <didtjdgns852@naver.com>
2024-02-27 10:03:45 +0000

    refactor: Refactor FormRoot component to use useFormState hook

--- app/ui/view/molecule/form/form-root.tsx
+++ app/ui/view/molecule/form/form-root.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useFormState } from 'react-dom';
 
-type FormRootProps = {};
+type State = {
+  message: string | null;
+  errors: Record<string, string>;
+};
 
-export function FormRoot({ children }: React.PropsWithChildren<FormRootProps>) {
+type FormRootProps = {
+  action: (prevState: State, formData: FormData) => State;
+};
+
+export function FormRoot({ action, children }: React.PropsWithChildren<FormRootProps>) {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(action, initialState);
+
   const render = () => {
     return React.Children.map(children, (child, index) => {
       return (
@@ -13,5 +24,5 @@ export function FormRoot({ children }: React.PropsWithChildren<FormRootProps>) {
     });
   };
 
-  return <form>{render()}</form>;
+  return <form action={dispatch}>{render()}</form>;
 }
