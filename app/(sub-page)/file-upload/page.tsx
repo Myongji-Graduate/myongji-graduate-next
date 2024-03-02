c3bc1ba3d15353fc17b9bffbc937d88a56a9ab8d
yougyung <youkyeongs@naver.com>
2024-03-02 20:36:13 +0900

    feat: implement file-upload manual

--- app/(sub-page)/file-upload/page.tsx
+++ app/(sub-page)/file-upload/page.tsx
@@ -1,10 +1,14 @@
+import UploadFile from '@/app/ui/view/molecule/upload-file';
 import ContentContainer from '../../ui/view/atom/content-container';
+import Manual from './components/manual';
 
 export default function Page() {
   return (
-    <ContentContainer className="flex flex-col items-center gap-8">
-      <div>요소1</div>
-      <div>요소2</div>
+    <ContentContainer className="grid place-items-center min-h-[70vh]">
+      <div className="flex flex-col gap-12">
+        <Manual />
+        <UploadFile />
+      </div>
     </ContentContainer>
   );
 }
