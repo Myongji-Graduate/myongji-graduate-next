07346d9f3903a5aebad5285b98e14604e02f1c46
gahyuun <pgh123166@naver.com>
2024-03-08 10:07:10 +0900

    feat: implement add lecture

--- app/ui/lecture/taken-lecture/taken-lecture-list.tsx
+++ app/ui/lecture/taken-lecture/taken-lecture-list.tsx
@@ -5,6 +5,8 @@ import { Table } from '../../view/molecule/table';
 import { useEffect, useState } from 'react';
 import Button from '../../view/atom/button/button';
 import { LectureInfo } from '@/app/type/lecture';
+import LectureSearch from '../lecture-search';
+import { SearchLectureInfo } from '../lecture-search/lecture-search-result-container';
 
 const headerInfo = ['수강년도', '수강학기', '과목코드', '과목명', '학점'];
 
@@ -20,6 +22,20 @@ export default function TakenLectureList({ data }: TakenLectureListProps) {
     setCustomLecture(customLecture.filter((lecture) => lecture.id !== id));
   };
 
+  const addLecture = (item: SearchLectureInfo) => {
+    setCustomLecture([
+      ...customLecture,
+      {
+        id: item.id,
+        year: 'CUSTOM',
+        semester: 'CUSTOM',
+        lectureCode: item.lectureCode,
+        lectureName: item.name,
+        credit: item.credit,
+      },
+    ]);
+  };
+
   const changeCustomizingState = () => {
     setIsCustomizing(!isCustomizing);
   };
@@ -31,27 +47,30 @@ export default function TakenLectureList({ data }: TakenLectureListProps) {
   }, [isCustomizing]);
 
   return (
-    <div className="w-[800px] flex flex-col gap-2">
-      {/* w-[800px]은 w-full로 변경 예정  */}
-      <TakenLectureLabel isCustomizing={isCustomizing} changeCustomizingState={changeCustomizingState} />
-      {isCustomizing ? (
-        <Table
-          headerInfo={headerInfo}
-          data={customLecture}
-          renderActionButton={(id: number) => (
-            <Button
-              label="삭제"
-              variant="list"
-              data-testid="taken-lecture-delete-button"
-              onClick={() => {
-                deleteLecture(id);
-              }}
-            />
-          )}
-        />
-      ) : (
-        <Table headerInfo={headerInfo} data={data} />
-      )}
+    <div className="w-[800px] flex flex-col gap-10">
+      {isCustomizing ? <LectureSearch handleAddButtonClick={addLecture} /> : null}
+      <div className="w-[800px] flex flex-col gap-2">
+        {/* w-[800px]은 w-full로 변경 예정  */}
+        <TakenLectureLabel isCustomizing={isCustomizing} changeCustomizingState={changeCustomizingState} />
+        {isCustomizing ? (
+          <Table
+            headerInfo={headerInfo}
+            data={customLecture}
+            renderActionButton={(id: number) => (
+              <Button
+                label="삭제"
+                variant="list"
+                data-testid="taken-lecture-delete-button"
+                onClick={() => {
+                  deleteLecture(id);
+                }}
+              />
+            )}
+          />
+        ) : (
+          <Table headerInfo={headerInfo} data={data} />
+        )}
+      </div>
     </div>
   );
 }
