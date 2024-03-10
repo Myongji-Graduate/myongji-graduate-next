7924d5dea936f00fb1e505b4f760287608831548
SeonghunYang <didtjdgns852@naver.com>
2024-03-10 04:58:01 +0000

    feat: Add TakenLecture component to Dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import RevenueChart from '@/app/ui/invoice/revenu-chart';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import TakenLecture from '@/app/ui/lecture/taken-lecture';
 
 export default async function Page() {
   return (
@@ -8,7 +9,7 @@ export default async function Page() {
       <h1 className={'mb-4 text-xl md:text-2xl'}>Dashboard</h1>
       <div className="mt-6">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
+          <TakenLecture />
         </Suspense>
       </div>
     </main>
