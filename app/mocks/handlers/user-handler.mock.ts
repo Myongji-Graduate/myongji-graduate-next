820bc236439f775a9f37040765b56d66ce9f2d71
SeonghunYang <didtjdgns852@naver.com>
2024-03-11 10:54:47 +0000

    feat: set Content-Type header in createUser function

--- app/mocks/handlers/user-handler.mock.ts
+++ app/mocks/handlers/user-handler.mock.ts
@@ -6,7 +6,6 @@ import { SignUpRequestBody } from '@/app/business/user/user.type';
 export const userHandlers = [
   http.post<never, SignUpRequestBody, never>(`${API_PATH.user}/sign-up`, async ({ request }) => {
     const userData = await request.json();
-    console.log('userdata', userData);
 
     mockDatabase.createUser(userData);
     await delay(2000);
