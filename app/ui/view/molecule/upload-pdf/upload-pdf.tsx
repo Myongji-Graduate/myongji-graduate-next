b3dac84a1452f9175addd48b3b2ba5bc4321ae47
yougyung <youkyeongs@naver.com>
2024-03-14 12:03:36 +0900

    style: add option truncate

--- app/ui/view/molecule/upload-pdf/upload-pdf.tsx
+++ app/ui/view/molecule/upload-pdf/upload-pdf.tsx
@@ -20,7 +20,7 @@ function UploadPdf() {
         className="relative p-2 m-auto w-96 flex flex-col justify-center items-center gap-2 border-dashed border-2 rounded-sm rounded-bl-xl border-light-blue-6 bg-light-blue-1 text-light-blue-6 max-lg:w-80"
       >
         <Image src={file ? checkedBox : uploadBox} width={40} height={28} className="mx-auto" alt="upload-button" />
-        <span className="text-center break-keep whitespace-pre-line max-w-48">
+        <span className="text-center break-keep whitespace-pre-line max-w-48 truncate">
           {file ? file.name : '마우스로 드래그 하거나 아이콘을 눌러 추가해주세요.'}
         </span>
         <input
