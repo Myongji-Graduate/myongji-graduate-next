4b1ff8576af030cfe127be16f51c0d2f153e0a45
SeonghunYang <didtjdgns852@naver.com>
2024-03-17 06:35:14 +0000

    refactor: Refactor sign-up page component

--- app/(sub-page)/sign-up/page.tsx
+++ app/(sub-page)/sign-up/page.tsx
@@ -1,11 +1,9 @@
-import SignUpForm from '@/app/ui/user/sign-up-form/sign-up-form';
+import SignUpContainer from './components/sign-up-container';
 
 export default async function Page() {
   return (
     <main>
-      <div className="w-96">
-        <SignUpForm />
-      </div>
+      <SignUpContainer />
     </main>
   );
 }
