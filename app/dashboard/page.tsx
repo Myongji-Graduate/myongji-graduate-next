ffbde277b94a3adfa495bd3a5b4ea59a2aa9255e
SeonghunYang <didtjdgns852@naver.com>
2024-03-19 13:50:22 +0000

    chore: Remove revenue chart from dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,11 +6,7 @@ export default async function Page() {
   return (
     <main>
       <h1 className={'mb-4 text-xl md:text-2xl'}>Dashboard</h1>
-      <div className="mt-6">
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
-        </Suspense>
-      </div>
+      <div className="mt-6"></div>
     </main>
   );
 }
