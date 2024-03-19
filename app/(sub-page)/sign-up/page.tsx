fa2a5ad68bdab8b53877f6dca19da68d0155f9e3
SeonghunYang <didtjdgns852@naver.com>
2024-03-19 13:57:29 +0000

    feat: Refactor sign-up page to use Suspense and LoadingSpinner

--- app/(sub-page)/sign-up/page.tsx
+++ app/(sub-page)/sign-up/page.tsx
@@ -1,10 +1,21 @@
 import ContentContainer from '@/app/ui/view/atom/content-container';
 import SignUpContainer from './components/sign-up-container';
+import { Suspense } from 'react';
+import LoadingSpinner from '@/app/ui/view/atom/loading-spinner';
 
-export default async function Page() {
+// Refactor: fallback 스켈레톤으로 대체
+export default function Page() {
   return (
     <ContentContainer className="md:w-[768px]">
-      <SignUpContainer />
+      <Suspense
+        fallback={
+          <div className="h-96">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <SignUpContainer />
+      </Suspense>
     </ContentContainer>
   );
 }
