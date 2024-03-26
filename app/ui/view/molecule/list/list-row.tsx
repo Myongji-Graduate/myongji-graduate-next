49174d314d962b5fc580613129aa7d2add37b6e4
gahyuun <pgh123166@naver.com>
2024-03-26 14:10:59 +0900

    chore: add hover style

--- app/ui/view/molecule/list/list-row.tsx
+++ app/ui/view/molecule/list/list-row.tsx
@@ -9,7 +9,7 @@ export function ListRow({ children, textColor = 'black' }: ListRowProps) {
   return (
     <div
       className={twMerge(
-        'border-solid border-gray-300 border-b-[1px] last:border-b-0 py-4 font-medium text-lg',
+        'border-solid border-gray-300 border-b-[1px] last:border-b-0 py-4 font-medium text-lg hover:bg-gray-100',
         textColor === 'red' ? 'text-red-500' : 'text-zinc-700',
       )}
     >
