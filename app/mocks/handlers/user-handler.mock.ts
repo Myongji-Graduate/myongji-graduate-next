77b0c1cbde000c456a02a7825a86117735fa0e9c
SeonghunYang <didtjdgns852@naver.com>
2024-03-27 12:04:49 +0000

    feat: Add sign-in functionality to user handler

--- app/mocks/handlers/user-handler.mock.ts
+++ app/mocks/handlers/user-handler.mock.ts
@@ -1,7 +1,7 @@
 import { HttpResponse, http, delay } from 'msw';
 import { API_PATH } from '../../business/api-path';
 import { mockDatabase } from '../db.mock';
-import { SignUpRequestBody } from '@/app/business/user/user.type';
+import { SignUpRequestBody, SignInRequestBody } from '@/app/business/user/user.type';
 
 export const userHandlers = [
   http.post<never, SignUpRequestBody, never>(`${API_PATH.user}/sign-up`, async ({ request }) => {
@@ -15,5 +15,21 @@ export const userHandlers = [
     }
 
     return HttpResponse.json({ status: 200 });
+  }),
+
+  http.post<never, SignInRequestBody, never>(`${API_PATH.auth}/sign-in`, async ({ request }) => {
+    const signInData = await request.json();
+
+    const isSuccess = mockDatabase.signIn(signInData);
+    await delay(500);
+
+    if (!isSuccess) {
+      return HttpResponse.json({ status: 400, message: '아이디 또는 비밀번호가 일치하지 않습니다.' }, { status: 400 });
+    }
+
+    return HttpResponse.json({
+      accessToken: 'fake-access-token',
+      refreshToken: 'fake-refresh-token',
+    });
   }),
 ];
