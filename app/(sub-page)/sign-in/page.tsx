1d46ae5697eac4e214e90eef368e9c895ba2bed5
SeonghunYang <didtjdgns852@naver.com>
2024-03-27 15:17:21 +0000

    fix: clear unnecessary annotation

--- app/(sub-page)/sign-in/page.tsx
+++ app/(sub-page)/sign-in/page.tsx
@@ -1,7 +1,6 @@
 import ContentContainer from '@/app/ui/view/atom/content-container';
 import SignInForm from '@/app/ui/user/sign-in-form/sign-in-form';
 
-// Refactor: fallback 스켈레톤으로 대체
 export default function Page() {
   return (
     <ContentContainer className="md:w-[768px]">
