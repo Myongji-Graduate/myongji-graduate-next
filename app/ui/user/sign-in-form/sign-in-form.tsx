b42608ba3c13ac0c7b3a35a4e45160e30ddc238a
SeonghunYang <didtjdgns852@naver.com>
2024-03-27 12:16:31 +0000

    test: Add sign-in form stories and update sign-in form component

--- app/ui/user/sign-in-form/sign-in-form.tsx
+++ app/ui/user/sign-in-form/sign-in-form.tsx
@@ -2,9 +2,13 @@
 import Form from '../../view/molecule/form';
 import { authenticate } from '@/app/business/user/user.command';
 
-export default function SignInForm() {
+interface SignInFormProps {
+  onNext?: () => void;
+}
+
+export default function SignInForm({ onNext }: SignInFormProps) {
   return (
-    <Form id="로그인" action={authenticate}>
+    <Form onSuccess={onNext} id="로그인" action={authenticate}>
       <Form.TextInput required={true} label="아이디" id="authId" placeholder="아이디를 입력하세요" />
       <Form.PasswordInput required={true} label="비밀번호" id="password" placeholder="비밀번호를 입력하세요" />
       <Form.SubmitButton label="로그인" position="center" variant="primary" />
