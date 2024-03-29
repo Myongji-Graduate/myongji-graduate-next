3090005d7e780b0ed661589adaa5aed8036ff483
gahyuun <pgh123166@naver.com>
2024-03-29 10:41:51 +0900

    feat: implement swipeable list

--- app/ui/view/molecule/table/index.tsx
+++ app/ui/view/molecule/table/index.tsx
@@ -3,11 +3,15 @@ import { ColType } from '../grid/grid-root';
 import List from '../list';
 import Grid from '../grid';
 import { ListRow } from '../list/list-root';
+import { SwipeableListItem } from 'react-swipeable-list';
+import '@/app/utils/style/reactswipeable-list.css';
+import SwipeableCustomList from '../list/swipeable-custom-list';
 
 interface TableProps<T extends ListRow> {
   headerInfo: string[];
   data: T[];
   renderActionButton?: (id: number) => JSX.Element;
+  swipeable?: boolean;
 }
 
 function isCol(cols: number | string): cols is ColType {
@@ -17,8 +21,9 @@ function isCol(cols: number | string): cols is ColType {
   return false;
 }
 
-export function Table<T extends ListRow>({ data, headerInfo, renderActionButton }: TableProps<T>) {
-  const cols = renderActionButton ? 'render-button' : headerInfo.length;
+export function Table<T extends ListRow>({ data, headerInfo, renderActionButton, swipeable = false }: TableProps<T>) {
+  const cols = renderActionButton && !swipeable ? 'render-button' : headerInfo.length;
+
   const render = (item: T, index: number) => {
     return (
       <List.Row key={index}>
@@ -33,10 +38,24 @@ export function Table<T extends ListRow>({ data, headerInfo, renderActionButton 
     );
   };
 
+  const swipealbeRender = (item: T, index: number) => {
+    return (
+      <SwipeableListItem trailingActions={renderActionButton ? renderActionButton(item.id) : null}>
+        <List.Row key={index}>
+          <Grid cols={isCol(cols) ? cols : 6}>
+            {Object.keys(item).map((key, index) => {
+              if (key === 'id') return null;
+              return <Grid.Column key={index}>{item[key]}</Grid.Column>;
+            })}
+          </Grid>
+        </List.Row>
+      </SwipeableListItem>
+    );
+  };
   return (
     <div className="flex flex-col gap-2.5 w-full" data-testid="table-data">
       <TableHeader headerInfo={headerInfo} cols={isCol(cols) ? cols : 6} />
-      <List data={data} render={render} />
+      {swipeable ? <SwipeableCustomList data={data} render={swipealbeRender} /> : <List data={data} render={render} />}
     </div>
   );
 }
