16c72c4bebcab7f8a6a9ddd47ee6b2059899fda6
gahyuun <pgh123166@naver.com>
2024-03-31 01:14:24 +0900

    chore: page -> MyPage

--- app/(sub-page)/my/page.tsx
+++ app/(sub-page)/my/page.tsx
@@ -1,9 +1,8 @@
 import LectureSearch from '@/app/ui/lecture/lecture-search';
 import TakenLecture from '@/app/ui/lecture/taken-lecture';
 import ContentContainer from '@/app/ui/view/atom/content-container';
-import React from 'react';
 
-export default function Page() {
+export default function MyPage() {
   return (
     <ContentContainer className="flex">
       <div className="hidden lg:w-[30%] lg:block">정보칸</div>
