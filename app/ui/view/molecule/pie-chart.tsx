623eb478d07e0c68e9828df0b974d0efe1aba5f2
yougyung <youkyeongs@naver.com>
2024-03-30 14:22:28 +0900

    style: change piechart size

--- app/ui/view/molecule/pie-chart.tsx
+++ app/ui/view/molecule/pie-chart.tsx
@@ -7,10 +7,10 @@ function PieChart({ percentage }: PieChartProp) {
 
   return (
     <div
-      className={`piechart relative aspect-square rounded-full bg-light-blue-1 w-[8rem] h-[8rem]`}
+      className={`piechart relative aspect-square rounded-full bg-light-blue-1 w-[9rem] h-[9rem]`}
       style={{ '--percentage': percentage }}
     >
-      <div className="absolute rounded-full bg-white text-primary font-bold text-xl w-[6rem] h-[6rem] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
+      <div className="absolute rounded-full bg-white text-light-blue-6 font-bold text-xl w-[6rem] h-[6rem] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
         {filterdPercentage}%
       </div>
     </div>
