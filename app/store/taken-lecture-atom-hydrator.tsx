d090cb933df135fea2c526de2b5e645a7aa65ddb
gahyuun <pgh123166@naver.com>
2024-04-03 16:10:34 +0900

    fix: solve  revalidate error

--- app/store/taken-lecture-atom-hydrator.tsx
+++ app/store/taken-lecture-atom-hydrator.tsx
@@ -1,12 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 import { swipeTakenLectureAtom, takenLectureAtom } from '@/app/store/custom-taken-lecture';
 import { LectureInfo } from '@/app/type/lecture';
+import { useSetAtom } from 'jotai';
 import { useHydrateAtoms } from 'jotai/utils';
+import { useEffect } from 'react';
 
 export default function TakenLectureAtomHydrator({
   initialValue,
   children,
 }: React.PropsWithChildren<{ initialValue: LectureInfo[] }>) {
+  const setTakenLectureState = useSetAtom(takenLectureAtom);
+  const setSwipeTakenLectureState = useSetAtom(swipeTakenLectureAtom);
+
+  useEffect(() => {
+    setTakenLectureState(initialValue);
+    setSwipeTakenLectureState(initialValue);
+  }, [initialValue]);
+
   useHydrateAtoms(new Map([[takenLectureAtom, initialValue]]));
   useHydrateAtoms(new Map([[swipeTakenLectureAtom, initialValue]]));
   return children;
