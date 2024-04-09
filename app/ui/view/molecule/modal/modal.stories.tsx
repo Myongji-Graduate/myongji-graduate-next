f824bb325c41645af1fd76fce5dc9f22a73a304f
yougyung <youkyeongs@naver.com>
2024-04-09 23:58:25 +0900

    refactor: update modal storybook

--- app/ui/view/molecule/modal/modal.stories.tsx
+++ app/ui/view/molecule/modal/modal.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Modal from './modal';
+import { MODAL_KEY, ModalKey } from '@/app/utils/key/modal.key';
 
 const meta = {
   title: 'ui/view/molecule/Modal',
@@ -9,14 +10,13 @@ const meta = {
 export default meta;
 
 interface ModalProps extends React.PropsWithChildren {
-  open: boolean;
-  onOpenChange: () => void;
+  modalKey: ModalKey;
 }
 
 export const Default: StoryObj<typeof meta> = {
   args: {
     children: <div className="text-xl">테스트 모달</div>,
-    open: true,
+    modalKey: MODAL_KEY.DIALOG_TEST,
   },
   render: (args: ModalProps) => <Modal {...args} />,
 };
--- app/utils/key/modal.key.ts
+++ app/utils/key/modal.key.ts
@@ -1,5 +1,6 @@
 export const MODAL_KEY = {
   RESULT_CATEGORY: 'RESULT_CATEGORY',
+  DIALOG_TEST: 'DIALOG_TEST',
 } as const;
 
 export type ModalKey = (typeof MODAL_KEY)[keyof typeof MODAL_KEY];
