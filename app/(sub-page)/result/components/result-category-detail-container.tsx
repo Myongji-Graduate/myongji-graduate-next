8a120207c313af18ee338915ce9d8391fe12f15e
yougyung <youkyeongs@naver.com>
2024-04-10 00:58:04 +0900

    refactor: update use Responsive component

--- app/(sub-page)/result/components/result-category-detail-container.tsx
+++ app/(sub-page)/result/components/result-category-detail-container.tsx
@@ -3,20 +3,21 @@ import { useMediaQuery } from 'usehooks-ts';
 import { DIALOG_KEY } from '@/app/utils/key/dialog.key';
 import Drawer from '../../../ui/view/molecule/drawer/drawer';
 import ResultCategoryDetailContent from '@/app/ui/result/result-category/result-category-detail-content';
+import Responsive from '@/app/ui/responsive';
 
 function ResultCategoryDetailContainer() {
-  const isDesktop = useMediaQuery('(min-width: 768px)');
   return (
     <>
-      {isDesktop ? (
+      <Responsive maxWidth={767}>
+        <Drawer drawerKey={DIALOG_KEY.RESULT_CATEGORY}>
+          <ResultCategoryDetailContent />
+        </Drawer>
+      </Responsive>
+      <Responsive minWidth={768}>
         <Modal modalKey={DIALOG_KEY.RESULT_CATEGORY}>
           <ResultCategoryDetailContent />
         </Modal>
-      ) : (
-        <Drawer modalKey={DIALOG_KEY.RESULT_CATEGORY}>
-          <ResultCategoryDetailContent />
-        </Drawer>
-      )}
+      </Responsive>
     </>
   );
 }
