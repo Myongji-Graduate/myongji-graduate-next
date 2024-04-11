2f68acdd5dae8529f18547c4e230e4511d11a947
gahyuun <pgh123166@naver.com>
2024-04-11 18:34:30 +0900

    feat: implement lecture add logic

--- app/store/custom-taken-lecture.ts
+++ app/store/custom-taken-lecture.ts
@@ -4,3 +4,5 @@ import { LectureInfo } from '../type/lecture';
 export const takenLectureAtom = atom<LectureInfo[]>([]);
 
 export const swipeTakenLectureAtom = atom<LectureInfo[]>([]);
+
+export const isAddedLectureAtom = atom<boolean>(false);
--- app/ui/lecture/taken-lecture/taken-lecture-label.tsx
+++ app/ui/lecture/taken-lecture/taken-lecture-label.tsx
@@ -1,14 +1,35 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client';
 import Link from 'next/link';
 import Button from '../../view/atom/button/button';
 import LabelContainer from '../../view/atom/label-container/label-container';
+import { useAtom, useSetAtom } from 'jotai';
+import { isAddedLectureAtom } from '@/app/store/custom-taken-lecture';
+import { useEffect } from 'react';
 
 export default function TakenLectureLabel() {
+  const setIsAddedLecture = useSetAtom(isAddedLectureAtom);
+
+  useEffect(() => {
+    return () => {
+      setIsAddedLecture(false);
+    };
+  }, []);
+
   return (
     <LabelContainer
       label="내 기이수 과목"
       rightElement={
         <div className="flex gap-2">
-          <Button label="과목 추가" variant="secondary" size="xs" data-testid="lecture-add-button" />
+          <Button
+            label="과목 추가"
+            variant="secondary"
+            size="xs"
+            data-testid="lecture-add-button"
+            onClick={() => {
+              setIsAddedLecture(true);
+            }}
+          />
           <Link href="/file-upload">
             <Button label="성적표 재업로드" variant="secondary" size="xs" />
           </Link>
