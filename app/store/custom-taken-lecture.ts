256d9d5952258fa1518db5612f52957c68ef0ef9
gahyuun <pgh123166@naver.com>
2024-04-12 11:40:26 +0900

    chore: delete isAddedLecture Atom

--- app/store/custom-taken-lecture.ts
+++ app/store/custom-taken-lecture.ts
@@ -4,5 +4,3 @@ import { LectureInfo } from '../type/lecture';
 export const takenLectureAtom = atom<LectureInfo[]>([]);
 
 export const swipeTakenLectureAtom = atom<LectureInfo[]>([]);
-
-export const isAddedLectureAtom = atom<boolean>(false);
