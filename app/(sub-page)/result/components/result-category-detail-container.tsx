97e9811ba9dc96dff3f92d741d327178f9b982cd
yougyung <youkyeongs@naver.com>
2024-04-13 11:09:23 +0900

    feat: result detail component connect mock data

--- app/(sub-page)/result/components/result-category-detail-container.tsx
+++ app/(sub-page)/result/components/result-category-detail-container.tsx
@@ -4,18 +4,21 @@ import { DIALOG_KEY } from '@/app/utils/key/dialog.key';
 import Drawer from '../../../ui/view/molecule/drawer/drawer';
 import ResultCategoryDetailContent from '@/app/ui/result/result-category/result-category-detail-content';
 import Responsive from '@/app/ui/responsive';
+import { fetchResultCategoryDetailInfo } from '@/app/business/result/result.query';
 
-function ResultCategoryDetailContainer() {
+async function ResultCategoryDetailContainer() {
+  const data = await fetchResultCategoryDetailInfo();
+
   return (
     <>
       <Responsive maxWidth={767}>
         <Drawer drawerKey={DIALOG_KEY.RESULT_CATEGORY}>
-          <ResultCategoryDetailContent />
+          <ResultCategoryDetailContent info={data} />
         </Drawer>
       </Responsive>
       <Responsive minWidth={768}>
         <Modal modalKey={DIALOG_KEY.RESULT_CATEGORY}>
-          <ResultCategoryDetailContent />
+          <ResultCategoryDetailContent info={data} />
         </Modal>
       </Responsive>
     </>
---
+++ app/business/result/result.query.ts
@@ -0,0 +1,33 @@
+import { SearchedLectureInfo } from '@/app/type/lecture';
+import { API_PATH } from '../api-path';
+
+export interface ResultCategoryDetailLectures {
+  categoryName: string;
+  totalCredits: number;
+  takenCredits: number;
+  takenLectures: SearchedLectureInfo[];
+  haveToLectures: SearchedLectureInfo[];
+  completed: boolean;
+}
+
+export interface ResultCategoryDetailInfo {
+  totalCredit: number;
+  takenCredit: number;
+  detailCategory: ResultCategoryDetailLectures[];
+  completed: boolean;
+}
+
+export interface ResultUserInfo {
+  studentNumber: string;
+  studentName: string;
+  studentCategory: 'NORMAL' | 'CHANGE_MAJOR' | 'DUAL_MAJOR' | 'SUB_MAJOR';
+  major: string;
+  totalCredit: number;
+  takenCredit: number;
+}
+
+export const fetchResultCategoryDetailInfo = async (): Promise<ResultCategoryDetailInfo> => {
+  const response = await fetch(API_PATH.resultCategoryDetailInfo);
+  const data = await response.json();
+  return data;
+};
