7206911534e1acf5057cc6e08ce82da4a03a154e
SeonghunYang <didtjdgns852@naver.com>
2024-04-13 01:57:56 +0000

    feat: Add protected page component

---
+++ app/(sub-page)/protected/page.tsx
@@ -0,0 +1,13 @@
+import { API_PATH } from '@/app/business/api-path';
+import { httpErrorHandler } from '@/app/utils/http/http-error-handler';
+
+export async function trigger() {
+  const response = await fetch(`${API_PATH.auth}/failure`);
+  const result = await response.json();
+  httpErrorHandler(response, result);
+}
+
+export default async function ProtectedPage() {
+  const data = await trigger();
+  return <div>Auth protected</div>;
+}
