c020a155d9838c86150aa3f016d01d6980feb1cc
gahyuun <pgh123166@naver.com>
2024-04-15 12:56:27 +0900

    chore: fix toggle function

--- app/ui/lecture/taken-lecture/taken-lecture-label.tsx
+++ app/ui/lecture/taken-lecture/taken-lecture-label.tsx
@@ -14,13 +14,7 @@ export default function TakenLectureLabel() {
       label="내 기이수 과목"
       rightElement={
         <div className="flex gap-2">
-          <Button
-            label="과목 추가"
-            variant="secondary"
-            size="xs"
-            data-testid="lecture-add-button"
-            onClick={() => toggle}
-          />
+          <Button label="과목 추가" variant="secondary" size="xs" data-testid="lecture-add-button" onClick={toggle} />
           <Link href="/file-upload">
             <Button label="성적표 재업로드" variant="secondary" size="xs" />
           </Link>
