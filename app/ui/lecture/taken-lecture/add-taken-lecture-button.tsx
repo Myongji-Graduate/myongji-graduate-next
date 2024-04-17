ae146955afd806f5373d9c872635dcaf11a5d5c3
gahyuun <pgh123166@naver.com>
2024-04-17 18:21:54 +0900

    feat: implement announce success of addTakenLecture

--- app/ui/lecture/taken-lecture/add-taken-lecture-button.tsx
+++ app/ui/lecture/taken-lecture/add-taken-lecture-button.tsx
@@ -1,18 +1,28 @@
 import { LectureInfo } from '@/app/type/lecture';
 import Form from '../../view/molecule/form';
 import { fetchAddTakenLecture } from '@/app/business/lecture/taken-lecture.command';
+import { useToast } from '../../view/molecule/toast/use-toast';
 
 interface AddTakenLectureButtonProps {
   lectureItem: LectureInfo;
   isTakenLecture: boolean;
 }
 export default function AddTakenLectureButton({ lectureItem, isTakenLecture }: AddTakenLectureButtonProps) {
+  const { toast } = useToast();
+
+  const announceSuccessOfAddTakenLecture = () => {
+    return toast({
+      title: '과목 추가에 성공했습니다',
+    });
+  };
   return (
     <Form
       id={`과목추가-${lectureItem.id}`}
       action={() => {
         return fetchAddTakenLecture(lectureItem.id);
       }}
+      messageControl="toast"
+      onSuccess={announceSuccessOfAddTakenLecture}
     >
       <Form.SubmitButton
         label="추가"
