cb0e1ae6586b3a6374d35b262fb60729f320e225
gahyuun <pgh123166@naver.com>
2024-04-17 17:02:54 +0900

    feat: add isSuccess attribute to formState

--- app/ui/view/molecule/form/form-submit-button.tsx
+++ app/ui/view/molecule/form/form-submit-button.tsx
@@ -7,8 +7,12 @@ import { useFormStatus } from 'react-dom';
 interface FormSubmitButtonProps {
   label: string;
   position?: 'left' | 'right' | 'center';
-  variant?: 'primary' | 'secondary';
+  variant?: 'primary' | 'secondary' | 'list';
   size?: ButtonSize;
+  disabled?: {
+    value: boolean;
+    control: boolean;
+  };
 }
 
 export function FormSubmitButton({
@@ -16,9 +20,14 @@ export function FormSubmitButton({
   position = 'right',
   variant = 'primary',
   size = 'md',
+  disabled = {
+    value: false, // disabled의 값
+    control: false, // disabled 를 control 하는지 (현재 form에서는 과목 추가에서만 disabled를 control)
+  },
 }: FormSubmitButtonProps) {
-  const { formId } = useContext(FormContext);
+  const { formId, isSuccess } = useContext(FormContext);
   const { pending } = useFormStatus();
+  const disabledValue = disabled.value ? true : disabled.control && isSuccess ? true : false;
 
   return (
     <div
@@ -36,6 +45,7 @@ export function FormSubmitButton({
         variant={variant}
         type="submit"
         label={label}
+        disabled={disabledValue}
       />
     </div>
   );
