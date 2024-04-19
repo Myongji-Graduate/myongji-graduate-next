9dd836431f3ee965e4a4e370aa013e349039ea00
gahyuun <pgh123166@naver.com>
2024-04-19 11:16:56 +0900

    feat: implement update taken lecture component

--- app/store/taken-lecture-atom-hydrator.tsx
+++ app/store/taken-lecture-atom-hydrator.tsx
@@ -2,21 +2,11 @@
 import { takenLectureAtom } from '@/app/store/custom-taken-lecture';
 import { TakenLectrueInfo } from '@/app/type/lecture';
 import { useHydrateAtoms } from 'jotai/utils';
-import { useEffect } from 'react';
-import { updateDialogAtom } from './dialog';
-import { useAtomValue, useSetAtom } from 'jotai';
 
 export default function TakenLectureAtomHydrator({
   initialValue,
   children,
 }: React.PropsWithChildren<{ initialValue: TakenLectrueInfo[] }>) {
-  const isLectureSearchOpen = useAtomValue(updateDialogAtom);
-  const setTakenLectures = useSetAtom(takenLectureAtom);
-
-  useEffect(() => {
-    setTakenLectures(initialValue);
-  }, [isLectureSearchOpen]);
-
   useHydrateAtoms(new Map([[takenLectureAtom, initialValue]]));
   return children;
 }
--- app/ui/lecture/taken-lecture/index.tsx
+++ app/ui/lecture/taken-lecture/index.tsx
@@ -2,14 +2,17 @@ import { fetchTakenLectures } from '@/app/business/lecture/taken-lecture.query';
 import TakenLectureList from './taken-lecture-list';
 import TakenLectureLabel from './taken-lecture-label';
 import TakenLectureAtomHydrator from '@/app/store/taken-lecture-atom-hydrator';
+import UpdateTakenLecture from './update-taken-lecture';
 
 export default async function TakenLecture() {
   const data = await fetchTakenLectures();
   return (
     <div className="flex flex-col gap-2">
       <TakenLectureAtomHydrator initialValue={data.takenLectures}>
-        <TakenLectureLabel />
-        <TakenLectureList />
+        <UpdateTakenLecture data={data.takenLectures}>
+          <TakenLectureLabel />
+          <TakenLectureList />
+        </UpdateTakenLecture>
       </TakenLectureAtomHydrator>
     </div>
   );
