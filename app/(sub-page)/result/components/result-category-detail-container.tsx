a995bf6fddeb54a96bfc7b594d5a63c5f5f823a6
yougyung <youkyeongs@naver.com>
2024-04-24 10:06:53 +0900

    refactor: refactor structure ResultCategoryDetailContainer

--- app/(sub-page)/result/components/result-category-detail-container.tsx
+++ app/(sub-page)/result/components/result-category-detail-container.tsx
@@ -1,28 +1,28 @@
 import Modal from '../../../ui/view/molecule/modal/modal';
-import { useMediaQuery } from 'usehooks-ts';
 import { DIALOG_KEY } from '@/app/utils/key/dialog.key';
 import Drawer from '../../../ui/view/molecule/drawer/drawer';
 import ResultCategoryDetailContent from '@/app/ui/result/result-category/result-category-detail-content';
 import Responsive from '@/app/ui/responsive';
 import { fetchResultCategoryDetailInfo } from '@/app/business/result/result.query';
 
-async function ResultCategoryDetailContainer({ category }: { category: string }) {
-  const data = await fetchResultCategoryDetailInfo(category);
-
+function responsiveDialog(children: React.ReactNode) {
   return (
     <>
       <Responsive maxWidth={767}>
-        <Drawer drawerKey={DIALOG_KEY.RESULT_CATEGORY}>
-          <ResultCategoryDetailContent info={data} />
-        </Drawer>
+        <Drawer drawerKey={DIALOG_KEY.RESULT_CATEGORY}>{children}</Drawer>
       </Responsive>
       <Responsive minWidth={768}>
-        <Modal modalKey={DIALOG_KEY.RESULT_CATEGORY}>
-          <ResultCategoryDetailContent info={data} />
-        </Modal>
+        <Modal modalKey={DIALOG_KEY.RESULT_CATEGORY}>{children}</Modal>
       </Responsive>
     </>
   );
 }
 
-export default ResultCategoryDetailContainer;
+export default async function ResultCategoryDetailContainer({ category }: { category: string }) {
+  const data = await fetchResultCategoryDetailInfo(category);
+  return responsiveDialog(<ResultCategoryDetailContent info={data} />);
+}
+
+export function ResultCategoryDetailContainerSkeleton() {
+  return responsiveDialog(<div className="flex flex-col items-center p-4  space-y-3"></div>);
+}
