1ccb7b37e99a6736224da64ea0ae61e9e5e8633f
SeonghunYang <didtjdgns852@naver.com>
2024-04-25 11:47:28 +0000

    feat: Add UserDeleteModal component

---
+++ app/ui/user/user-info-navigator/user-delete-modal.tsx
@@ -0,0 +1,10 @@
+import { DIALOG_KEY } from '@/app/utils/key/dialog.key';
+import Modal from '../../view/molecule/modal/modal';
+
+export default function UserDeleteModal() {
+  return (
+    <Modal modalKey={DIALOG_KEY.USER_DEELETE}>
+      <div className="text-center"></div>
+    </Modal>
+  );
+}
