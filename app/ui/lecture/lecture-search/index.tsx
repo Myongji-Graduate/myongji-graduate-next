c5e7c2e0018173b3391d9fc6d74226628862ec58
gahyuun <pgh123166@naver.com>
2024-04-28 11:52:26 +0900

    feat: implement search word atom

--- app/ui/lecture/lecture-search/index.tsx
+++ app/ui/lecture/lecture-search/index.tsx
@@ -1,25 +1,21 @@
+'use client';
+import { useAtomValue } from 'jotai';
 import LectureSearchBar from './lecture-search-bar';
-import LectureSearchResultContainer, { LectureSearchResultContainerSpinner } from './lecture-search-result-container';
+import LectureSearchResultContainer from './lecture-search-result-container';
+import { searchWordAtom } from '@/app/store/search-word';
 import EmptyDataContainer from './empty-data-container';
 import { Suspense } from 'react';
-import { LectureSearchParams } from '@/app/type/lecture';
 
-interface LectureSearchProps {
-  searchParams: LectureSearchParams;
-}
-export default function LectureSearch({ searchParams }: LectureSearchProps) {
-  const isSearchable = searchParams.type && searchParams.keyword && searchParams.keyword.length > 1;
-
+export default function LectureSearch() {
+  const searchWord = useAtomValue(searchWordAtom);
+  const searchable = searchWord.keyword && searchWord.keyword.length > 1;
   return (
     <div className="bg-white  w-full h-[500px] sm:h-[400px] z-[10] flex justify-center" data-testid="lecture-search">
       <div className="w-[800px] mx-auto my-7  flex flex-col gap-10 sm:gap-6">
         <LectureSearchBar />
-        {isSearchable ? (
-          <Suspense
-            key={`${searchParams.keyword}${searchParams.type}`}
-            fallback={<LectureSearchResultContainerSpinner />}
-          >
-            <LectureSearchResultContainer keyword={searchParams.keyword as string} type={searchParams.type as string} />
+        {searchable ? (
+          <Suspense fallback={<p>,,,loading</p>}>
+            <LectureSearchResultContainer />
           </Suspense>
         ) : (
           <EmptyDataContainer />
