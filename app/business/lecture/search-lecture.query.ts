76140ce3a8cc863473a9c028168fe26ab969edbc
gahyuun <pgh123166@naver.com>
2024-04-30 15:47:55 +0900

    chore: add headers

--- app/business/lecture/search-lecture.query.ts
+++ app/business/lecture/search-lecture.query.ts
@@ -1,11 +1,16 @@
 import { SearchedLectureInfo } from '@/app/type/lecture';
 import { API_PATH } from '../api-path';
 import axios from 'axios';
+import { cookies } from 'next/headers';
 
 export interface SearchLectures {
   lectures: SearchedLectureInfo[];
 }
 export const fetchSearchLectures = async (type: string, keyword: string) => {
-  const response = await axios<SearchLectures>(`${API_PATH.lectures}?type=${type}&&keyword=${keyword}`);
+  const response = await axios<SearchLectures>(`${API_PATH.lectures}?type=${type}&&keyword=${keyword}`, {
+    headers: {
+      Authorization: `Bearer ${cookies().get('accessToken')?.value}`,
+    },
+  });
   return response.data.lectures;
 };
