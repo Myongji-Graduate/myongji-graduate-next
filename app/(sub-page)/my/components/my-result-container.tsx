2f9a9a16c2fc14420d0d978495b7d13631b309fe
gahyuun <pgh123166@naver.com>
2024-05-02 12:51:40 +0900

    feat: implement my result container

---
+++ app/(sub-page)/my/components/my-result-container.tsx
@@ -0,0 +1,18 @@
+import UserCreditResult from '@/app/ui/user/total-credit-result';
+import Button from '@/app/ui/view/atom/button/button';
+import Link from 'next/link';
+import React from 'react';
+
+export default function MyResultContainer() {
+  return (
+    <div className="flex flex-col  justify-center gap-10">
+      <p className="font-bold sm:text-3xl text-2xl ml-1 sm:ml-0">마이페이지</p>
+      <UserCreditResult />
+      <div className="flex justify-center">
+        <Link href="/result">
+          <Button label="수강현황 자세히 보기" variant="primary" size="xs" />
+        </Link>
+      </div>
+    </div>
+  );
+}
