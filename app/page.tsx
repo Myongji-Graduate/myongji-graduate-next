adecc5307c4647d2b6187dfdeee9725a08505410
SeonghunYang <didtjdgns852@naver.com>
2024-05-02 05:18:25 +0000

    refactor: Refactor dashboard components and remove unused files

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link
-        href="/dashboard"
+        href="/my"
         className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
       >
-        <span>dashboard</span>
+        <span>my</span>
       </Link>
     </main>
   );
