a0b56c0e957292457a51fdddcdc3b5b8a2d295f4
gahyuun <pgh123166@naver.com>
2024-05-14 13:07:59 +0900

    feat: implement user credit result skeleton

--- app/(sub-page)/my/components/my-result-container.tsx
+++ app/(sub-page)/my/components/my-result-container.tsx
@@ -1,13 +1,16 @@
-import UserCreditResult from '@/app/ui/user/user-credit-result';
+import UserCreditResult from '@/app/ui/user/user-credit-result/user-credit-result';
+import UserCreditResultSkeleton from '@/app/ui/user/user-credit-result/user-credit-result-skeleton';
 import Button from '@/app/ui/view/atom/button/button';
 import Link from 'next/link';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 export default function MyResultContainer() {
   return (
     <div className="flex flex-col  justify-center gap-10">
       <p className="font-bold sm:text-3xl text-2xl ml-1 sm:ml-0">마이페이지</p>
-      <UserCreditResult />
+      <Suspense fallback={<UserCreditResultSkeleton />}>
+        <UserCreditResult />
+      </Suspense>
       <div className="flex justify-center">
         <Link href="/result">
           <Button label="수강현황 자세히 보기" variant="primary" size="xs" />
---
+++ app/ui/user/user-credit-result/user-credit-result-skeleton.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Skeleton from '../../view/atom/skeleton';
+
+export default function UserCreditResultSkeleton() {
+  return <Skeleton className="min-h-20 rounded-lg py-1 px-8 gap-3" />;
+}
