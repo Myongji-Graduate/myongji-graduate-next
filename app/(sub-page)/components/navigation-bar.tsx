dc758b5becaec955bee254b60e46cb0b9748c092
SeonghunYang <didtjdgns852@naver.com>
2024-05-16 07:02:19 +0000

    refactor: Refactor file paths for NavigationBar and SideNavigationBar components to match new folder structure

--- app/(sub-page)/components/navigation-bar.tsx
+++ app/(sub-page)/components/navigation-bar.tsx
@@ -3,13 +3,24 @@ import logo from '../../../public/assets/logo.svg';
 import Responsive from '../../ui/responsive';
 import SideNavigationBar from './side-navigation-bar';
 import UserInfoNavigator from '@/app/ui/user/user-info-navigator/user-info-navigator';
+import SignOutButton from '@/app/ui/user/user-info-navigator/sign-out-button';
+import UserDeleteButton from '@/app/ui/user/user-info-navigator/user-delete-button';
 
 export default function NavigationBar() {
   return (
     <div className="absolute flex justify-between items-center p-4 border-b-[1px] w-full z-2">
       <Image className="md:h-10 h-7 w-[110px] md:w-[150px]" width={150} height={100} src={logo} alt="main-logo" />
       <Responsive maxWidth={768}>
-        <SideNavigationBar header={<UserInfoNavigator />} />
+        <SideNavigationBar
+          header={<UserInfoNavigator />}
+          content={<div>콘텐츠</div>}
+          footer={
+            <div className="flex flex-col items-center space-y-2">
+              <SignOutButton />
+              <UserDeleteButton />
+            </div>
+          }
+        />
       </Responsive>
     </div>
   );
--- app/(sub-page)/components/side-navigation-bar.tsx
+++ app/(sub-page)/components/side-navigation-bar.tsx
@@ -1,19 +1,26 @@
 'use client';
 import { HamburgerMenuIcon } from '@radix-ui/react-icons';
-import { Sheet, SheetTrigger, SheetContent, SheetHeader } from '../../ui/view/molecule/sheet/sheet';
+import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetFooter } from '../../ui/view/molecule/sheet/sheet';
 
 type SideNavigationBarProps = {
   header: React.ReactNode;
+  footer: React.ReactNode;
+  content: React.ReactNode;
 };
 
-export default function SideNavigationBar({ header }: SideNavigationBarProps) {
+export default function SideNavigationBar({ header, content, footer }: SideNavigationBarProps) {
   return (
     <Sheet>
       <SheetTrigger className="h-6">
         <HamburgerMenuIcon className="w-6 h-6 text-white" />;
       </SheetTrigger>
       <SheetContent className="z-3">
-        <SheetHeader>{header}</SheetHeader>
+        <div className="flex h-full flex-col justify-between">
+          <SheetHeader>{header}</SheetHeader>
+          <div className="w-full h-1 rounded-full my-4 bg-gray-200" />
+          <div className="h-full">{content}</div>
+          <SheetFooter>{footer}</SheetFooter>
+        </div>
       </SheetContent>
     </Sheet>
   );
