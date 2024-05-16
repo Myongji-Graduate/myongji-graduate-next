3ae0765a0dcc488cc2bc3324afc7415ea42fe6c8
SeonghunYang <didtjdgns852@naver.com>
2024-05-16 06:10:45 +0000

    feat: Update SideNavigationBar component to include Sheet for additional functionality

--- app/ui/view/molecule/side-navigation-bar.tsx
+++ app/ui/view/molecule/side-navigation-bar.tsx
@@ -1,5 +1,22 @@
+'use client';
 import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription } from './sheet/sheet';
 
 export default function SideNavigationBar() {
-  return <HamburgerMenuIcon className="w-6 h-6 text-white" />;
+  return (
+    <Sheet>
+      <SheetTrigger className="h-6">
+        <HamburgerMenuIcon className="w-6 h-6 text-white" />;
+      </SheetTrigger>
+      <SheetContent className="z-3">
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your account and remove your data from our
+            servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
 }
