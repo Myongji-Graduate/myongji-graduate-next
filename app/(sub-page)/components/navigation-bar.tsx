01d29df3be1f0cea6558b23dd546c43af2fe18ec
SeonghunYang <didtjdgns852@naver.com>
2024-05-16 06:31:33 +0000

    refactor: Refactor file paths for NavigationBar and SideNavigationBar components

--- app/(sub-page)/components/navigation-bar.tsx
+++ app/(sub-page)/components/navigation-bar.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
-import logo from '../../../../public/assets/logo.svg';
+import logo from '../../../public/assets/logo.svg';
 import Responsive from '../../ui/responsive';
 import SideNavigationBar from './side-navigation-bar';
+import UserInfoNavigator from '@/app/ui/user/user-info-navigator/user-info-navigator';
 
 export default function NavigationBar() {
   return (
     <div className="absolute flex justify-between items-center p-4 border-b-[1px] w-full z-2">
       <Image className="md:h-10 h-7 w-[110px] md:w-[150px]" width={150} height={100} src={logo} alt="main-logo" />
       <Responsive maxWidth={768}>
-        <SideNavigationBar />
+        <SideNavigationBar header={<UserInfoNavigator />} />
       </Responsive>
     </div>
   );
--- app/(sub-page)/components/side-navigation-bar.tsx
+++ app/(sub-page)/components/side-navigation-bar.tsx
@@ -1,28 +1,19 @@
 'use client';
 import { HamburgerMenuIcon } from '@radix-ui/react-icons';
-import {
-  Sheet,
-  SheetTrigger,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetDescription,
-} from '../../ui/view/molecule/sheet/sheet';
+import { Sheet, SheetTrigger, SheetContent, SheetHeader } from '../../ui/view/molecule/sheet/sheet';
 
-export default function SideNavigationBar() {
+type SideNavigationBarProps = {
+  header: React.ReactNode;
+};
+
+export default function SideNavigationBar({ header }: SideNavigationBarProps) {
   return (
     <Sheet>
       <SheetTrigger className="h-6">
         <HamburgerMenuIcon className="w-6 h-6 text-white" />;
       </SheetTrigger>
       <SheetContent className="z-3">
-        <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your account and remove your data from our
-            servers.
-          </SheetDescription>
-        </SheetHeader>
+        <SheetHeader>{header}</SheetHeader>
       </SheetContent>
     </Sheet>
   );
