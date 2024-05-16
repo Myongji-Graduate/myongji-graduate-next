2bc8fdbdefa29ec346fe37833104199c820a7b21
yougyung <youkyeongs@naver.com>
2024-05-16 12:59:12 +0900

    chore: change result-category skeleton length

--- app/ui/result/result-category/result-category.skeleton.tsx
+++ app/ui/result/result-category/result-category.skeleton.tsx
@@ -6,7 +6,7 @@ async function ResultCategorySkeleton() {
     <div
       className={cn('absolute grid grid-cols-2 gap-2 top-[30rem] w-full', 'md:max-w-[700px] md:gap-10 md:top-[33rem]')}
     >
-      {Array.from({ length: 3 }).map((_, index) => (
+      {Array.from({ length: 4 }).map((_, index) => (
         <ResultCategoryCardSkeleton key={index} />
       ))}
     </div>
