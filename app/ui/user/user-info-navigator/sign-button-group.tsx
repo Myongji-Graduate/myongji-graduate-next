d1612980202a0fd2463728f673fdc2bae2311f33
SeonghunYang <didtjdgns852@naver.com>
2024-05-20 08:19:28 +0000

    refactor: Refactor SignButtonGroup component to import Button and Link from specific paths

--- app/ui/user/user-info-navigator/sign-button-group.tsx
+++ app/ui/user/user-info-navigator/sign-button-group.tsx
@@ -2,8 +2,6 @@ import { auth } from '@/app/business/user/user.query';
 import SignOutButton from './sign-out-button';
 import UserDeleteButton from './user-delete-button';
 import SignInLinkButton from './sign-in-link-button';
-import Button from '../../view/atom/button/button';
-import Link from 'next/link';
 
 export default async function SignButtonGroup() {
   const userInfo = await auth();
