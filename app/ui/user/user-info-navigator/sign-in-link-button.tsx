e6a148ca0ded56b589cde24b2e7767f7985b2633
SeonghunYang <didtjdgns852@naver.com>
2024-05-20 08:15:23 +0000

    refactor: Refactor SignButtonGroup component to import Button and Link from specific paths

--- app/ui/user/user-info-navigator/sign-in-link-button.tsx
+++ app/ui/user/user-info-navigator/sign-in-link-button.tsx
@@ -1,11 +1,15 @@
 'use client';
+import useDialog from '@/app/hooks/useDialog';
 import Button from '../../view/atom/button/button';
 import { useRouter } from 'next/navigation';
+import { DIALOG_KEY } from '@/app/utils/key/dialog-key.util';
 
 export default function SignInLinkButton() {
+  const { close } = useDialog(DIALOG_KEY.SIDE_NAVIGATION);
   const router = useRouter();
 
   const handleToLoginButtonClick = () => {
+    close();
     router.push('/sign-in');
   };
   return <Button onClick={handleToLoginButtonClick} size="sm" variant="text" label="로그인" />;
