f2af2eb992a9c12adc02323ff443a41fcf794fda
SeonghunYang <didtjdgns852@naver.com>
2024-05-23 14:46:40 +0000

    chore: add Cypress test runner script

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+export default {
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1280,
+    viewportHeight: 844,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
