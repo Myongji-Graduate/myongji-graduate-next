71abd47c0f0ec512d9ff0f5a2fa0819194eac927
gahyuun <pgh123166@naver.com>
2024-05-24 10:16:07 +0900

    docs: documentation avatar story

--- app/ui/view/atom/avatar/avatar.stories.tsx
+++ app/ui/view/atom/avatar/avatar.stories.tsx
@@ -5,6 +5,25 @@ import Avatar from './avatar';
 const meta = {
   title: 'ui/view/atom/Avatar',
   component: Avatar,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+- src 값으로 이미지 경로를 할당할 수 있습니다.\n
+`,
+      },
+    },
+  },
+  argTypes: {
+    src: {
+      description: '이미지 경로를 설정합니다',
+      table: {
+        type: { summary: 'AvatarSrc' },
+        defaultValue: { summary: './assets/profile-image.png' },
+      },
+    },
+  },
 } as Meta<typeof Avatar>;
 
 export default meta;
