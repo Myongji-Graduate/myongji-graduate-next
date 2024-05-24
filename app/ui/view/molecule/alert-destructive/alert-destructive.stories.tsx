3a7e86870783e4f4aae5e8ef3252ecf63e12bc47
gahyuun <pgh123166@naver.com>
2024-05-24 10:41:04 +0900

    docs: documentation alert destructive story

--- app/ui/view/molecule/alert-destructive/alert-destructive.stories.tsx
+++ app/ui/view/molecule/alert-destructive/alert-destructive.stories.tsx
@@ -5,6 +5,18 @@ import AlertDestructive from './alert-destructive';
 const meta = {
   title: 'ui/view/molecule/AlertDestructive',
   component: AlertDestructive,
+  tags: ['autodocs'],
+  componentSubtitle: '실패시 노출하는 alert component입니다',
+  parameters: {
+    docs: {
+      description: {
+        component: `
+- title 값으로 alert의 제목을 설정할 수 있습니다.\n
+-description 값으로 alert 내용을 작성할 수 있고 필수값입니다\n
+`,
+      },
+    },
+  },
 } as Meta<typeof AlertDestructive>;
 
 export default meta;
