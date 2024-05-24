85e38cf8689be2ea372ce6616e9391980b646fae
gahyuun <pgh123166@naver.com>
2024-05-24 15:20:23 +0900

    docs: documentation drawer story

--- app/ui/view/molecule/drawer/drawer.stories.tsx
+++ app/ui/view/molecule/drawer/drawer.stories.tsx
@@ -1,10 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Drawer from './drawer';
 import { DIALOG_KEY, DialogKey } from '@/app/utils/key/dialog-key.util';
+import useDialog from '@/app/hooks/useDialog';
 
 const meta = {
   title: 'ui/view/molecule/Drawer',
   component: Drawer,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: `
+- drawer 키 값으로 drawer 노출을 control 할 수 있습니다 \n
+- onClose 값으로 drawer가 닫혔을 때 실행할 함수입니다.\n
+`,
+      },
+    },
+  },
 } satisfies Meta<typeof Drawer>;
 
 export default meta;
@@ -15,7 +27,7 @@ interface DrawerProps extends React.PropsWithChildren {
 
 export const Default: StoryObj<typeof meta> = {
   args: {
-    children: <div className="text-xl">Drawer</div>,
+    children: <div className="text-xl h-40">Drawer</div>,
     drawerKey: DIALOG_KEY.DIALOG_TEST,
   },
   render: (args: DrawerProps) => <Drawer {...args} />,
