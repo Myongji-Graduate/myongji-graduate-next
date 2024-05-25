ae062bad7e7aa3cad7a89f4c62763fea86c832a9
gahyuun <pgh123166@naver.com>
2024-05-25 10:56:04 +0900

    chore: delete example code

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default async function Page() {
-  return (
-    <main>
-      <h1 className={'mb-4 text-xl md:text-2xl'}>Dashboard</h1>
-      <div className="mt-6"></div>
-    </main>
-  );
-}
