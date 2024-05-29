0166ce6db7dfa02a92f6b7edcc190415183a6c78
yougyung <youkyeongs@naver.com>
2024-05-29 16:10:59 +0900

    feat: implement imageCard common component

---
+++ app/ui/view/molecule/image-card/image-card.stories.tsx
@@ -0,0 +1,20 @@
+import feature1 from '@/public/assets/tutorial/tutorial-feature1.png';
+import type { Meta, StoryObj } from '@storybook/react';
+import ImageCard, { ImageCardProps } from './image-card';
+
+const meta = {
+  title: 'ui/view/molecule/ImageCard',
+  component: ImageCard,
+} as Meta<typeof ImageCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    image: feature1,
+    title: 'first',
+    content: '강의 커스텀을 통한 졸업 사정 예측',
+  },
+  render: (args: ImageCardProps) => <ImageCard {...args} />,
+};
