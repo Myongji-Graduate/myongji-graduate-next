d1003492d7bf3154856ae2b25cbec0abf3db5143
SeonghunYang <didtjdgns852@naver.com>
2024-05-29 06:03:40 +0000

    feat: Update homepage link to result page

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -1,16 +1,18 @@
 describe('The Home Page', () => {
-  it('successfully loads', () => {
-    //   cy.visit('/')
-
+  it('a', () => {
     cy.login('admin', 'admin');
+    cy.visit('/my');
+    //  마이페이지 → 결과페이지
+    cy.dataCy('result-page-link').click();
   });
 
   it('critical path', () => {
     // 1. 메인 -> 로그인 -> 마이 페이지
-    cy.login('admin', 'admin');
     // 2. 마이 페이지 -> 결과 페이지
-
+    cy.login('admin', 'admin');
+    cy.visit('/my');
     //  마이페이지 → 결과페이지
+    cy.dataCy('result-page-link').click();
 
     // 1
   });
