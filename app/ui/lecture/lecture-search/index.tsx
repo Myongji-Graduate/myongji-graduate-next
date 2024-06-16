e6540341d1ff4ffc1cada7a393fb48f1da5a4a7c
gahyuun <pgh123166@naver.com>
2024-06-16 19:04:03 +0900

    style: modify lecture search drawer style

--- app/ui/lecture/lecture-search/index.tsx
+++ app/ui/lecture/lecture-search/index.tsx
@@ -26,7 +26,7 @@ export default function LectureSearch() {
   const searchable = searchWord.keyword && searchWord.keyword.length > 1;
 
   return (
-    <div className="bg-white  w-full h-[500px] sm:h-[400px] z-[10] flex justify-center" data-testid="lecture-search">
+    <div className="bg-white w-full h-[520px] sm:h-[420px] z-[10] flex justify-center" data-testid="lecture-search">
       <div className="w-[800px] mx-auto my-7  flex flex-col gap-10 sm:gap-6">
         <LectureSearchBar />
         {searchable ? (
