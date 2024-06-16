5757a7e9932307e5212e852de5a946137027fa55
gahyuun <pgh123166@naver.com>
2024-06-16 18:42:54 +0900

    fix: add server action async

--- app/business/services/auth.ts
+++ app/business/services/auth.ts
@@ -1,6 +1,7 @@
 'use server';
 import { cookies } from 'next/headers';
 
-export const getToken = (): string | undefined => {
+export const getToken = async (): Promise<string | undefined> => {
   return cookies().get('accessToken')?.value;
 };
+// server action은 async를 써야함
