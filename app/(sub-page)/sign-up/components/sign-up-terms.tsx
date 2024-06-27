9ce5fbd39dbf3f6be307dd4d7c3814cc4626f4f8
SeonghunYang <didtjdgns852@naver.com>
2024-06-27 08:06:58 +0000

    [web] refactor: Update sign-up terms page layout and content

--- app/(sub-page)/sign-up/components/sign-up-terms.tsx
+++ app/(sub-page)/sign-up/components/sign-up-terms.tsx
@@ -1,4 +1,5 @@
 import Button from '@/app/ui/view/atom/button/button';
+import TitleBox from '@/app/ui/view/molecule/title-box/title-box';
 
 interface SignUpTermProps {
   onNext?: () => void;
@@ -11,44 +12,60 @@ export default function SignUpTerm({ onNext }: SignUpTermProps) {
   };
 
   return (
-    <div className="max-w-2xl mx-auto my-8 p-6 bg-white rounded-lg shadow-md">
-      <h1 className="text-3xl font-bold text-center mb-6">알림독의 안내문</h1>
-      <ul className="list-disc space-y-4 text-sm">
-        <li>
-          현재 저희 기능은 한국-한국은 아니라 한국-외국도 가능합니다. 각사별에서 수하인 없더라도 저희가 받는데까지는
-          무관합니다만, 꼭 관세사무소와 협의하세요!
-          <ul className="list-disc ml-6 mt-2">
-            <li>대상: 국외발송, 북부발송, 사회복지대상, ICT용품대상, 일반대상, 미래용품대상(확인)</li>
-            <li>발송: 16 ~ 22시발</li>
+    <div className="max-w-2xl mx-auto my-8 p-6 bg-white ">
+      <TitleBox title="약관동의">
+        <div className="border p-6 pl-8 rounded-lg">
+          <ul className="list-disc space-y-4 text-sm ">
+            <li>
+              현재 검사 가능한 학과-학번은 아래과 같습니다. 검사대상에 속하지 않다면 검사가 불가능합니다. 꼭
+              검사대상인지 확인하세요!
+              <ul className="list-disc ml-6 mt-2">
+                <li>
+                  대학: 경영대학, 법과대학, 사회과학대학, ICT융합대학, 인문대학,{' '}
+                  <span className="text-red-400">미래융합대학(불가)</span>
+                </li>
+                <li>학번: 16 ~ 22학번</li>
+              </ul>
+            </li>
+            <li>
+              교직, 다전공, 연계전공, 편입, 전과, 재외국민/외국인전형에 해당하는 사용자는 검사 기준이 따로 설정되지 않아
+              검사가 불가능합니다.
+            </li>
+            <li>검사를 위해선 성적표를 직접 업로드해야하므로 PC환경에서 진행하는 것을 권장합니다.</li>
+            <li>
+              검사 기준은 최신버전 학사안내문(2023.07.24) 반영하여 설정되었으며, 학사안내문은 매년 개편되므로 자신이
+              알고 있는 구버전과 다를 수 있습니다.
+              <ul className="list-disc ml-6 mt-2">
+                <li>
+                  <a
+                    target={'_blank'}
+                    className="text-blue-500 underline	 "
+                    href="https://www.mju.ac.kr/mjukr/257/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGbWp1a3IlMkYxNDMlMkYyMDI1ODAlMkZhcnRjbFZpZXcuZG8lM0ZwYWdlJTNEMSUyNnNyY2hDb2x1bW4lM0RzaiUyNnNyY2hXcmQlM0QlRUQlOTUlOTklRUMlODIlQUMlMjZiYnNDbFNlcSUzRCUyNmJic09wZW5XcmRTZXElM0QlMjZyZ3NCZ25kZVN0ciUzRCUyNnJnc0VuZGRlU3RyJTNEJTI2aXNWaWV3TWluZSUzRGZhbHNlJTI2aXNWaWV3JTNEdHJ1ZSUyNnBhc3N3b3JkJTNEJTI2"
+                  >
+                    명지대학교 학사안내문 참고 링크
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              본 서비스 정보는 공식적인 효력을 갖지 않으며,{' '}
+              <b>정확한 졸업사정결과를 위해서는 소속 단과대 교학팀에서의 확인을 권장합니다.</b>
+            </li>
+            <li>
+              저장된 사용자 데이터베이스는 익명화되어 저장되고 과목추천 및 교양과목 통계에만 사용되며, 익명 다른 용도로
+              사용되지 않습니다.
+            </li>
+            <li>졸업요건 기준이 잘못 설정되었거나, 오류발생 시 우측 하단 채널톡으로 피드백 부탁드립니다.</li>
           </ul>
-        </li>
-        <li>
-          교직, 디자인, 연계개발, 물품, 전자, 자원관리/회계지원에 해당하는 사용자는 각사 기준에 따른 선정되지 않아 각사
-          별 관리하는데요.
-        </li>
-        <li>검사를 위해서 선적품을 직접 연락드려야만 PC화면에서 진행하는 것을 권장합니다.</li>
-        <li>
-          검사 기준은 최신버전 확인내역(2023.07.24) 반영하여 선정되었으며, 학사내역은 매년 개편되므로 자사이 외고 있는
-          구버전과 다를 수 있습니다.
-          <ul className="list-disc ml-6 mt-2">
-            <li>문자대항: 학사내역은 확인 클릭</li>
-          </ul>
-        </li>
-        <li>
-          본 서비스 정보는 공식적인 확인을 전제 않으며, 정확한 증상조사결과를 위해 서류 또는 담당과 교류해야할 사항을
-          잊지 않습니다.
-        </li>
-        <li>
-          전자문 서화지 데이터베이스는 의무화되어 저희가 고유축적 및 교육과정 등에서 사용되며, 어떤 다른 용도로 사용
-          되지 않습니다.
-        </li>
-        <li>특허요건 기준이 전문 선정되었거나, 오류발생 시 우측 하단 채팅창으로 또는 담당 부서로문의합니다.</li>
-      </ul>
+        </div>
+      </TitleBox>
+
       <div className="mt-8 flex justify-center">
         <Button
           onClick={handleAgreeButtonClick}
-          className="ml-4 bg-blue-500 text-white py-2 px-4 rounded-full"
-          label={'알림독의 안내문'}
+          // className="ml-4 bg-blue-500 text-white py-2 px-4 rounded-full"
+          size={'md'}
+          label={'동의합니다'}
         />
       </div>
     </div>
