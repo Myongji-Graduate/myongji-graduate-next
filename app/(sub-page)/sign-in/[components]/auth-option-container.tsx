db1bbe74766253d6feea418a699eb5e14c7318b7
yougyung <youkyeongs@naver.com>
2024-06-29 23:16:01 +0900

    feat: implement authOptionContainer

---
+++ app/(sub-page)/sign-in/[components]/auth-option-container.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+function AuthOptionContainer() {
+  return (
+    <div className="text-gray-6 flex gap-2">
+      <div className="after:content-['|'] after:pl-2">
+        <Link href="/find-id">아이디 찾기</Link>
+      </div>
+      <div className="after:content-['|'] after:pl-2">
+        <Link href="/find-pw">비밀번호 재설정</Link>
+      </div>
+      <Link href="/sign-up">회원가입하기</Link>
+    </div>
+  );
+}
+
+export default AuthOptionContainer;
--- app/(sub-page)/sign-in/page.tsx
+++ app/(sub-page)/sign-in/page.tsx
@@ -1,10 +1,12 @@
 import SignInForm from '@/app/ui/user/sign-in-form/sign-in-form';
 import ContentContainer from '@/app/ui/view/atom/content-container/content-container';
+import AuthOptionContainer from './[components]/auth-option-container';
 
 export default function Page() {
   return (
     <ContentContainer className="md:w-[768px]">
       <SignInForm />
+      <AuthOptionContainer />
     </ContentContainer>
   );
 }
