76f763f774d2af0ac092bb371c179caf60026399
SeonghunYang <didtjdgns852@naver.com>
2024-06-29 05:28:39 +0000

    refactor: Update sign-up success page layout and content

--- app/(sub-page)/sign-up/components/sign-up-success.tsx
+++ app/(sub-page)/sign-up/components/sign-up-success.tsx
@@ -6,17 +6,16 @@ import Link from 'next/link';
 export default function SignUpSuccess() {
   return (
     <>
-      <TitleBox title={'회원가입 완료'}>
-        <div className="h-[260px] text-2xl font-bold w-2/4 flex flex-col justify-center space-y-2">
-          <div>회원가입이 완료되었습니다</div>
-          <div>로그인 후 졸업 사정 결과를 확인해보세요!</div>
-          <div className="pt-6">
-            <Link href="/sign-in">
-              <Button size={'md'} label={'로그인 하기'} />
-            </Link>
-          </div>
+      <TitleBox title={'회원가입 완료'} />
+      <div className="h-[260px] text-2xl font-bold  flex flex-col items-center justify-center space-y-2">
+        <div>회원가입이 완료되었습니다</div>
+        <div className="text-xl font-medium">로그인 후 졸업 사정 결과를 확인해보세요!</div>
+        <div className="pt-6">
+          <Link href="/sign-in">
+            <Button size={'md'} label={'로그인 하기'} />
+          </Link>
         </div>
-      </TitleBox>
+      </div>
     </>
   );
 }
