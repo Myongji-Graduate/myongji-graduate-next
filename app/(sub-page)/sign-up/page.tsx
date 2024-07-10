9cc47c625f6a5f072fe370cca3bb02a49b0c8dd1
SeonghunYang <didtjdgns852@naver.com>
2024-07-10 06:18:53 +0000

    refactor: Replace loading spinner with sign-up form skeleton

--- app/(sub-page)/sign-up/page.tsx
+++ app/(sub-page)/sign-up/page.tsx
@@ -1,19 +1,12 @@
 import SignUpContainer from './components/sign-up-container';
 import { Suspense } from 'react';
 import ContentContainer from '@/app/ui/view/atom/content-container/content-container';
-import LoadingSpinner from '@/app/ui/view/atom/loading-spinner/loading-spinner';
+import SignUpFormSkeleton from '@/app/ui/user/sign-up-form/sign-up-form.skeleton';
 
-// Refactor: fallback 스켈레톤으로 대체
 export default function Page() {
   return (
     <ContentContainer className="md:w-[768px] xl:w-[960px]">
-      <Suspense
-        fallback={
-          <div className="h-96">
-            <LoadingSpinner />
-          </div>
-        }
-      >
+      <Suspense fallback={<SignUpFormSkeleton />}>
         <SignUpContainer />
       </Suspense>
     </ContentContainer>
---
+++ app/ui/user/sign-up-form/sign-up-form.skeleton.tsx
@@ -0,0 +1,30 @@
+import Skeleton from '@/app/utils/skeleton';
+
+export default function SignUpFormSkeleton() {
+  return (
+    <div className="p-6">
+      <div className="space-y-4">
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+        <Skeleton className="h-4 w-1/2" />
+        <Skeleton className="h-4 w-1/4" />
+      </div>
+    </div>
+  );
+}
