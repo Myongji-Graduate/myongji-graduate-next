4c7510a4087468f4d0aa9195d859dc0ce310b7b4
yougyung <youkyeongs@naver.com>
2024-07-13 21:10:51 +0900

    feat: implement find-auth form skeleton

--- app/(sub-page)/find-password/page.tsx
+++ app/(sub-page)/find-password/page.tsx
@@ -1,20 +1,14 @@
 import ContentContainer from '@/app/ui/view/atom/content-container/content-container';
 import TitleBox from '@/app/ui/view/molecule/title-box/title-box';
 import FindPasswordContainer from './[components]/find-password-container';
-import LoadingSpinner from '@/app/ui/view/atom/loading-spinner/loading-spinner';
 import { Suspense } from 'react';
+import FindIdFormSkeleton from '../../ui/user/find-id-form/find-id-form.skeleton';
 
 function FindPasswordPage() {
   return (
     <ContentContainer>
       <TitleBox title="비밀번호 재설정" />
-      <Suspense
-        fallback={
-          <div className="h-96">
-            <LoadingSpinner />
-          </div>
-        }
-      >
+      <Suspense fallback={<FindIdFormSkeleton />}>
         <FindPasswordContainer />
       </Suspense>
     </ContentContainer>
