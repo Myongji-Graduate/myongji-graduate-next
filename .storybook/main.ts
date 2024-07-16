61b9c70bb2743151636ca47175a4dd39fbaf6378
SeonghunYang <didtjdgns852@naver.com>
2024-07-16 09:10:21 +0000

    chore: add @storybook/addon-a11y for accessibility testing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,6 +13,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/nextjs',
