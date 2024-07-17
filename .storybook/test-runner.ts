afe24d9edd44eae1a8684fb56eff2b83ce2b829c
SeonghunYang <didtjdgns852@naver.com>
2024-07-17 03:14:38 +0000

    chore: Update test runner configuration for accessibility testing and dependencies

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,5 +1,7 @@
 import type { TestRunnerConfig } from '@storybook/test-runner';
-import { injectAxe, checkA11y } from 'axe-playwright';
+import { getStoryContext } from '@storybook/test-runner';
+
+import { injectAxe, checkA11y, configureAxe } from 'axe-playwright';
 
 /*
  * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
@@ -9,7 +11,15 @@ const config: TestRunnerConfig = {
   async preVisit(page) {
     await injectAxe(page);
   },
-  async postVisit(page) {
+  async postVisit(page, context) {
+    // Get the entire context of a story, including parameters, args, argTypes, etc.
+    const storyContext = await getStoryContext(page, context);
+
+    // Apply story-level a11y rules
+    await configureAxe(page, {
+      rules: storyContext.parameters?.a11y?.config?.rules,
+    });
+
     await checkA11y(page, '#storybook-root', {
       detailedReport: true,
       detailedReportOptions: {
