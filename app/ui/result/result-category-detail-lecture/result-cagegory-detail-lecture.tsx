5682e15e28481dd0db30e25dc7993045f3467f01
yougyung <youkyeongs@naver.com>
2024-08-27 22:08:58 +0900

    feat: add announce text in none lecture case

--- app/ui/result/result-category-detail-lecture/result-cagegory-detail-lecture.tsx
+++ app/ui/result/result-category-detail-lecture/result-cagegory-detail-lecture.tsx
@@ -3,6 +3,7 @@ import AnnounceMessageBox from '@/app/ui/view/molecule/announce-message-box/anno
 import LabelContainer from '@/app/ui/view/atom/label-container/label-container';
 import { cn } from '@/app/utils/shadcn/utils';
 import { ResultCategoryDetailLecturesResponse } from '@/app/store/querys/result';
+import CompletedImage from '@/public/assets/completed-category.png';
 
 const headerInfo = ['과목코드', '과목명', '학점'];
 
@@ -15,6 +16,7 @@ function ResultCagegoryDetailLecture({ detailCategory, isTakenLecture }: ResultC
   const { categoryName, totalCredit, takenCredit, takenLectures, haveToLectures, completed } = detailCategory;
 
   const showCompleted = !isTakenLecture && completed;
+  const showNoneLecture = isTakenLecture && takenLectures.length === 0;
 
   return (
     <div className={cn('my-4 flex flex-col gap-4 min-h-48', 'md:min-h-60')}>
@@ -27,9 +29,12 @@ function ResultCagegoryDetailLecture({ detailCategory, isTakenLecture }: ResultC
         }
       />
       {showCompleted ? (
-        <AnnounceMessageBox message="해당 파트의 졸업요건을 충족하셨습니다!" />
+        <AnnounceMessageBox message="해당 파트의 졸업요건을 충족하셨습니다!" background_image={CompletedImage} />
       ) : (
-        <Table headerInfo={headerInfo} data={isTakenLecture ? takenLectures : haveToLectures} />
+        <>
+          <Table headerInfo={headerInfo} data={isTakenLecture ? takenLectures : haveToLectures} />
+          {showNoneLecture && <AnnounceMessageBox message="이수한 과목 정보가 존재하지 않습니다." />}
+        </>
       )}
     </div>
   );
