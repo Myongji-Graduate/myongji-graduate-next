456d58f513901b5e17c919364f97b1b27e6d49ef
gahyuun <pgh123166@naver.com>
2024-09-02 13:17:02 +0900

    fix: implement processChapel

--- app/ui/lecture/taken-lecture/index.tsx
+++ app/ui/lecture/taken-lecture/index.tsx
@@ -1,15 +1,29 @@
-import { fetchTakenLectures } from '@/app/business/services/lecture/taken-lecture.query';
+import { fetchTakenLectures, TakenLectureInfoResponse } from '@/app/business/services/lecture/taken-lecture.query';
 import TakenLectureList from './taken-lecture-list';
 import TakenLectureLabel from './taken-lecture-label';
 import TakenLectureAtomHydrator from '@/app/store/stores/taken-lecture-atom-hydrator';
 import UpdateTakenLecture from './update-taken-lecture';
 
+const processChapel = (takenLectures: TakenLectureInfoResponse[]) => {
+  return takenLectures.map((lecture) => {
+    return {
+      id: lecture.id,
+      year: lecture.year,
+      semester: lecture.semester,
+      lectureCode: lecture.lectureCode,
+      lectureName: lecture.lectureName,
+      credit: lecture.lectureName === '채플' ? 0.5 : lecture.credit,
+    };
+  });
+};
 export default async function TakenLecture() {
   const data = await fetchTakenLectures();
+  const takenLectures = processChapel(data.takenLectures);
+
   return (
     <div className="flex flex-col gap-2">
-      <TakenLectureAtomHydrator initialValue={data.takenLectures}>
-        <UpdateTakenLecture data={data.takenLectures}>
+      <TakenLectureAtomHydrator initialValue={takenLectures}>
+        <UpdateTakenLecture data={takenLectures}>
           <TakenLectureLabel />
           <TakenLectureList />
         </UpdateTakenLecture>
