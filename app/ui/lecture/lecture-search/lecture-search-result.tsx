8bc604a7cecb8837450accd90c898c1aaabce304
gahyuun <pgh123166@naver.com>
2024-09-02 13:37:23 +0900

    refactor: display UI when no search results

--- app/ui/lecture/lecture-search/lecture-search-result.tsx
+++ app/ui/lecture/lecture-search/lecture-search-result.tsx
@@ -3,9 +3,22 @@ import List from '@/app/ui/view/molecule/list';
 import Grid from '@/app/ui/view/molecule/grid';
 import AddTakenLectureButton from '../taken-lecture/add-taken-lecture-button';
 import { useFetchSearchLecture } from '@/app/store/querys/lecture';
+import { useAtomValue } from 'jotai';
+import { searchWordAtom } from '@/app/store/stores/search-word';
+
+const emptyDataRender = (searchWord: string) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <div className="text-md font-medium text-gray-400 text-center whitespace-pre-wrap">
+        <span className="text-light-blue-7 text-lg font-semibold">{searchWord}</span> 에 대한 검색 결과가 없습니다.
+      </div>
+    </div>
+  );
+};
 
 export default function LectureSearchResult() {
   const { data } = useFetchSearchLecture();
+  const searchWord = useAtomValue(searchWordAtom);
 
   const renderAddActionButton = (item: SearchedLectureInfoResponse, taken: boolean) => {
     return <AddTakenLectureButton lectureItem={item} isTaken={taken} />;
@@ -28,5 +41,14 @@ export default function LectureSearchResult() {
     );
   };
 
-  return <List data={data} render={render} isScrollList={true} />;
+  return (
+    <List
+      data={data}
+      render={render}
+      isScrollList={true}
+      emptyDataRender={() => {
+        return emptyDataRender(searchWord.keyword as string);
+      }}
+    />
+  );
 }
