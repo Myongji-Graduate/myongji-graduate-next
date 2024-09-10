b4107dd89244a5e28065daeb0c60f653c599acc6
yougyung <youkyeongs@naver.com>
2024-09-10 13:29:06 +0900

    style: change steyl sing-up success

--- app/(sub-page)/sign-up/components/sign-up-success.tsx
+++ app/(sub-page)/sign-up/components/sign-up-success.tsx
@@ -1,15 +1,15 @@
 import Button from '@/app/ui/view/atom/button/button';
 import TitleBox from '@/app/ui/view/molecule/title-box/title-box';
 import Link from 'next/link';
-
-// 내용이랑 스타일은 mock인 상태입니다.
+import Image from 'next/image';
+import maru from '../../../../public/assets/maru.png';
 export default function SignUpSuccess() {
   return (
     <>
       <TitleBox title={'회원가입 완료'} />
-      <div className="h-[260px] text-2xl font-bold  flex flex-col items-center justify-center space-y-2">
-        <div>회원가입이 완료되었습니다</div>
-        <div className="text-xl font-medium">로그인 후 졸업 사정 결과를 확인해보세요!</div>
+      <div className="text-2xl font-bold flex flex-col items-center justify-center space-y-2">
+        <Image src={maru} width={150} height={150} alt="main-book-background" className="max-md:w-24 mt-8" />
+        <div className="text-base md:text-lg font-light">이제 졸업 사정 결과를 확인해보세요!</div>
         <div className="pt-6">
           <Link href="/sign-in">
             <Button size={'md'} label={'로그인 하기'} />
