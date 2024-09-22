2c3ef24d5343adba388e45ffd8da25d62c1f5fd2
SeonghunYang <didtjdgns852@naver.com>
2024-09-22 10:53:50 +0000

    feat: fetch-ax instance에 인터셉터로 cookie를 가져오도록 구현

--- app/utils/http/instance.ts
+++ app/utils/http/instance.ts
@@ -5,17 +5,18 @@ import { cookies } from 'next/headers';
 export const instance = fetchAx.create({
   headers: {
     'Content-Type': 'application/json',
-    // Authorization: `Bearer ${cookies().get('accessToken')?.value}`, 서버라서 안되지 않을까? 공유 인스턴스 처럼.. 근데 그러면 기존에는 어캐 디는거야>
   },
   responseRejectedInterceptor: (error) => {
     fetchAxErrorHandler(error);
   },
   requestInterceptor: (config) => {
     const accessToken = cookies().get('accessToken')?.value;
-    // if (accessToken) {
-    //   config.headers;
-    // }
-    console.log(accessToken);
+    if (accessToken) {
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${accessToken}`,
+      };
+    }
     return config;
   },
 });
