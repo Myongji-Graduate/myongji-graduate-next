2b2b75e0f7bbc2e515ca3881a2b279dd4a29067a
yougyung <youkyeongs@naver.com>
2024-09-26 23:21:33 +0900

    fix: UserDeleteModal move in page level

--- app/ui/user/user-info-navigator/user-delete-button.tsx
+++ app/ui/user/user-info-navigator/user-delete-button.tsx
@@ -2,7 +2,6 @@
 import Button from '../../view/atom/button/button';
 import { DIALOG_KEY } from '@/app/utils/key/dialog-key.util';
 import useDialog from '@/app/hooks/useDialog';
-import UserDeleteModal from './user-delete-modal';
 
 export default function UserDeleteButton() {
   const { open: userDeleteDialogOpen } = useDialog(DIALOG_KEY.USER_DELETE);
@@ -13,10 +12,5 @@ export default function UserDeleteButton() {
     userDeleteDialogOpen();
   };
 
-  return (
-    <>
-      <Button onClick={handleModalToggle} size="sm" variant="text" label="회원탈퇴하기" />
-      <UserDeleteModal />
-    </>
-  );
+  return <Button onClick={handleModalToggle} size="sm" variant="text" label="회원탈퇴하기" />;
 }
