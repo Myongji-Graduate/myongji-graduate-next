decf0738923af47a00b5b74891ce4bfd62f35f65
yougyung <youkyeongs@naver.com>
2024-09-26 23:45:26 +0900

    fix: prevent rerendering in ChannelTalk

--- app/utils/global/channel-talk.tsx
+++ app/utils/global/channel-talk.tsx
@@ -1,12 +1,15 @@
 'use client';
 import * as ChannelService from '@channel.io/channel-web-sdk-loader';
+import { useEffect } from 'react';
 
 export default function ChannelTalk() {
-  if (typeof window !== 'undefined') {
-    ChannelService.loadScript();
-    ChannelService.boot({
-      pluginKey: process.env.NEXT_PUBLIC_CHANNELTALK_PLUGIN ?? '',
-    });
-  }
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      ChannelService.loadScript();
+      ChannelService.boot({
+        pluginKey: process.env.NEXT_PUBLIC_CHANNELTALK_PLUGIN ?? '',
+      });
+    }
+  }, []);
   return <></>;
 }
