e73d642eada9ece1750475f859f85f772dad9838
gahyuun <pgh123166@naver.com>
2024-10-30 00:27:11 +0900

    feat: migration fetch ax

--- app/business/services/lecture/taken-lecture.command.ts
+++ app/business/services/lecture/taken-lecture.command.ts
@@ -1,13 +1,10 @@
 'use server';
 import { FormState } from '@/app/ui/view/molecule/form/form-root';
 import { API_PATH } from '../../api-path';
-import { httpErrorHandler } from '@/app/utils/http/http-error-handler';
 import { BadRequestError } from '@/app/utils/http/http-error';
-import { revalidateTag } from 'next/cache';
-import { TAG } from '@/app/utils/http/tag';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
-import { getToken } from '../auth';
+import { instance } from '@/app/utils/api/instance';
 
 export const registerUserGrade = async (prevState: FormState, formData: FormData) => {
   const parsingText = await parsePDFtoText(formData);
@@ -45,23 +42,13 @@ export const parsePDFtoText = async (formData: FormData) => {
 
 export const deleteTakenLecture = async (lectureId: number) => {
   try {
-    const response = await fetch(`${API_PATH.takenLectures}/${lectureId}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${cookies().get('accessToken')?.value}`,
-      },
+    await instance.delete(`${API_PATH.takenLectures}/${lectureId}`, {
+      responseType: 'text',
     });
-    if (response.ok) {
-      revalidateTag(TAG.GET_TAKEN_LECTURES);
-      return {
-        isSuccess: true,
-      };
-    } else {
-      return {
-        isSuccess: false,
-      };
-    }
+
+    return {
+      isSuccess: true,
+    };
   } catch (error) {
     if (error instanceof BadRequestError) {
       return {
@@ -74,30 +61,30 @@ export const deleteTakenLecture = async (lectureId: number) => {
 };
 
 export const addTakenLecture = async (lectureId: number) => {
-  const token = await getToken();
-  const response = await fetch(API_PATH.takenLectures, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({ lectureId }),
-  });
-  // delete taken lecture과 비슷한 이유로 코드 수정
-  if (response.ok) {
-    revalidateTag(TAG.GET_TAKEN_LECTURES);
+  try {
+    await instance.post(
+      API_PATH.takenLectures,
+      { lectureId },
+      {
+        responseType: 'text',
+      },
+    );
     return {
       isSuccess: true,
       isFailure: false,
       validationError: {},
       message: '과목 추가에 성공했습니다',
     };
-  } else {
-    return {
-      isSuccess: false,
-      isFailure: true,
-      validationError: {},
-      message: '과목 추가에 실패했습니다',
-    };
+  } catch (error) {
+    if (error instanceof BadRequestError) {
+      return {
+        isSuccess: false,
+        isFailure: true,
+        validationError: {},
+        message: '과목 추가에 실패했습니다',
+      };
+    } else {
+      throw error;
+    }
   }
 };
--- app/business/services/lecture/taken-lecture.query.ts
+++ app/business/services/lecture/taken-lecture.query.ts
@@ -1,6 +1,5 @@
+import { instance } from '@/app/utils/api/instance';
 import { API_PATH } from '../../api-path';
-import { TAG } from '@/app/utils/http/tag';
-import { cookies } from 'next/headers';
 
 export interface TakenLecturesResponse {
   totalCredit: number;
@@ -17,13 +16,7 @@ export interface TakenLectureInfoResponse {
   credit: number;
 }
 
-export const fetchTakenLectures = async (): Promise<TakenLecturesResponse> => {
-  const response = await fetch(API_PATH.takenLectures, {
-    next: { tags: [TAG.GET_TAKEN_LECTURES] },
-    headers: {
-      Authorization: `Bearer ${cookies().get('accessToken')?.value}`,
-    },
-  });
-  const data = await response.json();
-  return data;
+export const fetchTakenLectures = async () => {
+  const response = await instance.get<TakenLecturesResponse>(API_PATH.takenLectures);
+  return response.data;
 };
