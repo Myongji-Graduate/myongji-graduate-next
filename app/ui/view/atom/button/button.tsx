d283831f500b9630d9900038d93ce35d32753ace
SeonghunYang <didtjdgns852@naver.com>
2024-11-27 04:43:50 +0000

    fix: text variant button의 색 명도를 수정
    텍스트 색과 배경색의 색상 대비율을 높이기 위해 수정

--- app/ui/view/atom/button/button.tsx
+++ app/ui/view/atom/button/button.tsx
@@ -20,7 +20,7 @@ export const ButtonVariants = cva(`flex justify-center items-center`, {
       outlined: 'rounded-[100px] text-primary border-solid border-[1px] border-primary',
       dark: 'bg-dark rounded-[100px] text-white border-0 hover:bg-dark-hover',
       secondary: 'bg-white rounded-[100px] border-solid border-[1px] border-gray-6 hover:bg-white-hover',
-      text: 'font-medium text-slate-400 text-sm hover:text-slate-600',
+      text: 'font-medium text-slate-500 text-sm hover:text-slate-600',
       list: 'py-1 px-3 bg-blue-600 rounded-[7px] text-white leading-5 font-medium text-base hover:bg-blue-700',
     },
     size: {
