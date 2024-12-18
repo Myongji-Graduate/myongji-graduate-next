579b68dc4a3fb486cf1e6caa569c90ee2d37252e
SeonghunYang <didtjdgns852@naver.com>
2024-12-18 04:29:12 +0000

    chore: add sitemap.xml for improved SEO and better site indexing

--- middleware.ts
+++ middleware.ts
@@ -77,5 +77,5 @@ async function retryAuth(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|mockServiceWorker|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api|sitemap|mockServiceWorker|_next/static|_next/image|.*\\.png$).*)'],
 };
