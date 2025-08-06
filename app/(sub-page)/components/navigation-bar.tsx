3f99b6d79187426b632b226882ab9315d38f4cb6
xxxjinn <tlsguswls319@naver.com>
2025-08-06 17:37:13 +0900

    refactor: 네비게이션 바에서 유저 정보가 있다면 탈퇴 버튼을, 그렇지 않으면 해당 부분을 비움

--- app/(sub-page)/components/navigation-bar.tsx
+++ app/(sub-page)/components/navigation-bar.tsx
@@ -7,15 +7,23 @@ import SignButtonGroup from '@/app/ui/user/user-info-navigator/sign-button-group
 import Link from 'next/link';
 import NavigationItems from './navigation-items';
 import UserDeleteModal from '@/app/ui/user/user-info-navigator/user-delete-modal';
+import { auth } from '@/app/business/services/user/user.query';
+import UserDeleteButton from '@/app/ui/user/user-info-navigator/user-delete-button';
 
-export default function NavigationBar() {
+export default async function NavigationBar() {
+  const userInfo = auth();
+
   return (
     <div className="absolute flex justify-between items-center p-2.5 border-b-[1px] w-full z-2">
       <Link href={'/'}>
         <Image className="md:h-10 h-7 w-[110px] md:w-[150px]" width={150} height={100} src={logo} alt="main-logo" />
       </Link>
       <Responsive maxWidth={1023}>
-        <SideNavigationBar header={<UserInfoNavigator />} content={<NavigationItems />} footer={<SignButtonGroup />} />
+        <SideNavigationBar
+          header={<UserInfoNavigator />}
+          content={<NavigationItems />}
+          footer={(await userInfo) ? <UserDeleteButton /> : <></>}
+        />
       </Responsive>
       <Responsive minWidth={1024}>
         <NavigationItems />
