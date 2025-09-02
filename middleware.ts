b3f75b65c76f7d9eba8ee235f6e54b6834165759
xxxjinn <tlsguswls319@naver.com>
2025-09-02 16:19:35 +0900

    feat: allow init user path 배열에 시간표 페이지 추가

--- middleware.ts
+++ middleware.ts
@@ -28,7 +28,7 @@ async function getAuth(request: NextRequest): Promise<{
 
 const allowedOnlyGuestPath = ['/sign-in', '/sign-up', '/find-password', '/find-id'];
 const allowedGuestPath = ['/', '/tutorial', '/anonymous', '/anonymous/result', ...allowedOnlyGuestPath];
-const allowInitUserPath = ['/', '/tutorial', '/grade-upload', '/anonymous', '/anonymous/result'];
+const allowInitUserPath = ['/', '/tutorial', '/grade-upload', '/anonymous', '/anonymous/result', '/timetable'];
 
 function isAllowedGuestPath(path: string, strict: boolean = false) {
   const allowedPath = strict ? allowedOnlyGuestPath : allowedGuestPath;
