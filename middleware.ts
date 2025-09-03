71d3193db334d87449af1af4a610457eebb91486
심유진 <syj11243@naver.com>
2025-09-03 19:23:16 +0900

    fix: middleware 충돌 해결

--- middleware.ts
+++ middleware.ts
@@ -28,7 +28,16 @@ async function getAuth(request: NextRequest): Promise<{
 
 const allowedOnlyGuestPath = ['/sign-in', '/sign-up', '/find-password', '/find-id'];
 const allowedGuestPath = ['/', '/tutorial', '/anonymous', '/anonymous/result', '/subjects', ...allowedOnlyGuestPath];
-const allowInitUserPath = ['/', '/tutorial', '/grade-upload', '/anonymous', '/anonymous/result', '/subjects'];
+
+const allowInitUserPath = [
+  '/',
+  '/tutorial',
+  '/grade-upload',
+  '/anonymous',
+  '/anonymous/result',
+  '/subjects',
+  '/timetable',
+];
 
 function isAllowedGuestPath(path: string, strict: boolean = false) {
   const allowedPath = strict ? allowedOnlyGuestPath : allowedGuestPath;
