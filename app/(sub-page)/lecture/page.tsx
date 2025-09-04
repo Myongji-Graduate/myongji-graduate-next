7ed18297fc515c89336f339151703570cf365226
심유진 <syj11243@naver.com>
2025-09-04 17:09:47 +0900

    fix: description 수정

--- app/(sub-page)/lecture/page.tsx
+++ app/(sub-page)/lecture/page.tsx
@@ -6,7 +6,7 @@ import LectureContents from './components/lecture-contents';
 
 export const metadata: Metadata = {
   title: '필수 과목 조회',
-  description: '학과별 필수 과목을 조회하고 인기순으로 확인해보세요',
+  description: '학과별 필수 과목을 조회하고, 인기순으로 비교해보세요!',
 };
 
 export default function LecturePage() {
