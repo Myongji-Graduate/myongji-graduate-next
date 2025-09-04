c475ae298c9f7dc4bd152bdc8e07c66b72f098b8
xxxjinn <tlsguswls319@naver.com>
2025-09-05 00:42:24 +0900

    style: 시간표 생성 페이지 TitleBox 사용

--- app/(sub-page)/timetable/components/create-timetable/create-timetable.tsx
+++ app/(sub-page)/timetable/components/create-timetable/create-timetable.tsx
@@ -1,15 +1,13 @@
+import TitleBox from '@/app/ui/view/molecule/title-box/title-box';
 import NotCompletedLectureContainer from './not-completed-lecture/not-completed-lecture-container';
 
 function CreateTimetable() {
   return (
-    <div className="flex flex-col gap-5 md:gap-6  pb-4 md:pb-6">
-      <div className="flex flex-col gap-1 border-b-2 pb-4 ">
-        <p className="font-bold sm:text-3xl text-2xl sm:ml-0">시간표 생성</p>
-        <p className="text-gray-400">미이수 과목들로 시간표를 만들고 관리해보세요.</p>
-      </div>
-      <div>
-        <NotCompletedLectureContainer />
-      </div>
+    <div className="flex flex-col gap-6 md:gap-8 pb-4 md:pb-6">
+      <TitleBox title="시간표 생성">
+        <p>미이수 과목들로 시간표를 만들고 관리해보세요!</p>
+      </TitleBox>
+      <NotCompletedLectureContainer />
     </div>
   );
 }
