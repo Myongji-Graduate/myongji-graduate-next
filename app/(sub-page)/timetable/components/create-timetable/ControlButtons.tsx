d0623f926f4ac94e11c75fd621d659efb812f684
xxxjinn <tlsguswls319@naver.com>
2025-09-05 15:39:11 +0900

    style: 버튼 사이의 gap 수정

--- app/(sub-page)/timetable/components/create-timetable/ControlButtons.tsx
+++ app/(sub-page)/timetable/components/create-timetable/ControlButtons.tsx
@@ -10,7 +10,7 @@ function ControlButtons() {
   return (
     <div className="flex justify-between">
       <Button label="과목 추가" size="xs" tabIndex={isOpen ? -1 : 0} onClick={open} />
-      <div className="flex gap-3">
+      <div className="flex gap-2">
         <Button label="초기화" size="xs" variant="outlined" />
         <Button label="저장" size="xs" variant="outlined" />
         <Button label="삭제" size="xs" variant="outlined" />
