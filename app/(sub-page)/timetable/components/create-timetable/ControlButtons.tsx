8efadd3dd9e7e1170b26ce3dbbf681a58815d3bc
xxxjinn <tlsguswls319@naver.com>
2025-09-05 15:38:39 +0900

    feat: 과목 추가 버튼에 drawer open 동작 달기

--- app/(sub-page)/timetable/components/create-timetable/ControlButtons.tsx
+++ app/(sub-page)/timetable/components/create-timetable/ControlButtons.tsx
@@ -1,9 +1,15 @@
+'use client';
+
+import useDialog from '@/app/hooks/useDialog';
 import Button from '@/app/ui/view/atom/button/button';
+import { DIALOG_KEY } from '@/app/utils/key/dialog-key.util';
 
 function ControlButtons() {
+  const { isOpen, open } = useDialog(DIALOG_KEY.TIMETABLE_LECTURE_SEARCH);
+
   return (
     <div className="flex justify-between">
-      <Button label="과목 추가" size="xs" />
+      <Button label="과목 추가" size="xs" tabIndex={isOpen ? -1 : 0} onClick={open} />
       <div className="flex gap-3">
         <Button label="초기화" size="xs" variant="outlined" />
         <Button label="저장" size="xs" variant="outlined" />
