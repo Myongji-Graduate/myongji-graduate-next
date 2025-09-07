bb0dcd30cca361d7403ce108ec0f952fa4a3215f
xxxjinn <tlsguswls319@naver.com>
2025-09-08 01:02:50 +0900

    refactor: 과목 추가 컴포넌트 파일 분리

---
+++ app/(sub-page)/timetable/components/create-timetable/lecture/lecture-row-mobile.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Responsive from '@/app/ui/responsive';
+import { ListRow } from '@/app/ui/view/molecule/list/list-root';
+
+function LectureRowMobile({ item }: { item: ListRow }) {
+  return (
+    <Responsive maxWidth={999}>
+      <div className="flex flex-col gap-1 p-3 border-b-gray-200">
+        <div className="flex gap-1">
+          <p className="text-sm text-gray-600">[{item.lectureCode}]</p>
+          <p className="font-semibold">{item.name}</p>
+        </div>
+        <p className="text-sm">
+          {item.professor} · {item.credit}학점
+        </p>
+        <div className="text-sm flex gap-3">
+          <p>
+            {item.day1} {item.time1}
+          </p>
+          {item.day2 && item.time2 && (
+            <p>
+              {item.day2} {item.time2}
+            </p>
+          )}
+        </div>
+        <p className="text-sm text-gray-500">{item.lectureRoom}</p>
+        {item.note && <p className="text-xs text-blue-600">{item.note}</p>}
+      </div>
+    </Responsive>
+  );
+}
+
+export default LectureRowMobile;
