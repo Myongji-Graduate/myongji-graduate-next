a81d97222d5b627187da1c8dc90e5d9292ea26a4
xxxjinn <tlsguswls319@naver.com>
2025-09-09 14:44:12 +0900

    feat: 이수구분 필터 추가

--- app/(sub-page)/timetable/components/create-timetable/lecture/timetable-lecture-filters.tsx
+++ app/(sub-page)/timetable/components/create-timetable/lecture/timetable-lecture-filters.tsx
@@ -6,10 +6,11 @@ import { SelectRoot } from '@/app/ui/view/molecule/select/select-root';
 import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import Responsive from '@/app/ui/responsive';
 import { MIN_WIDTH, MAX_WIDTH } from '@/app/ui/timetable/create-timetable-constants';
+import { RESULT_CATEGORY_KO } from '@/app/utils/key/result-category.key';
 
 function TimetableLectureFilters() {
   const CampusSelect = (
-    <div className="w-[100px]">
+    <div className="w-[90px]">
       <SelectRoot placeholder="캠퍼스">
         <SelectItem placeholder="인문" value="인문" />
         <SelectItem placeholder="자연" value="자연" />
@@ -18,11 +19,34 @@ function TimetableLectureFilters() {
   );
 
   const CompletionSelect = (
-    <div className="w-[120px]">
+    <div className="w-[110px]">
       <SelectRoot placeholder="이수 여부">
         <SelectItem placeholder="미이수" value="미이수" />
         <SelectItem placeholder="이수" value="이수" />
         <SelectItem placeholder="전체" value="전체" />
+      </SelectRoot>
+    </div>
+  );
+
+  const categories = [
+    RESULT_CATEGORY_KO.COMMON_CULTURE,
+    RESULT_CATEGORY_KO.CORE_CULTURE,
+    RESULT_CATEGORY_KO.PRIMARY_MANDATORY_MAJOR,
+    RESULT_CATEGORY_KO.PRIMARY_ELECTIVE_MAJOR,
+    RESULT_CATEGORY_KO.PRIMARY_BASIC_ACADEMICAL_CULTURE,
+    RESULT_CATEGORY_KO.NORMAL_CULTURE,
+    RESULT_CATEGORY_KO.FREE_ELECTIVE,
+    RESULT_CATEGORY_KO.CHAPEL,
+    RESULT_CATEGORY_KO.TRANSFER_CHRISTIAN,
+  ];
+
+  const CategorySelect = (
+    <div className="w-[120px] min-w-[120px]">
+      <SelectRoot placeholder="이수구분">
+        <SelectItem placeholder="전체" value="all" />
+        {categories.map((category, index) => (
+          <SelectItem key={index} placeholder={category} value={category} />
+        ))}
       </SelectRoot>
     </div>
   );
@@ -35,6 +59,7 @@ function TimetableLectureFilters() {
     <>
       {CampusSelect}
       {CompletionSelect}
+      {CategorySelect}
     </>
   );
 
