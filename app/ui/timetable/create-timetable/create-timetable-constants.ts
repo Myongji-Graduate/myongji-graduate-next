b47673a1f0ea1f00fec973dfc1036e8ea6f9e07d
xxxjinn <tlsguswls319@naver.com>
2025-09-15 21:15:53 +0900

    feat: 과목명/교수명 검색 모달 구현

--- app/ui/timetable/create-timetable/create-timetable-constants.ts
+++ app/ui/timetable/create-timetable/create-timetable-constants.ts
@@ -1,3 +1,11 @@
 /** 스타일링 깨지는 기준 상수 */
 export const MAX_WIDTH = 395;
 export const MIN_WIDTH = 396;
+
+/**과목명, 교수명 라디오버튼 옵션 */
+export const SEARCH_OPTIONS = [
+  { label: '과목명', value: '과목명' },
+  { label: '교수명', value: '교수명' },
+];
+
+export const DEFAULT_SEARCH_OPTION = SEARCH_OPTIONS[0].value;
