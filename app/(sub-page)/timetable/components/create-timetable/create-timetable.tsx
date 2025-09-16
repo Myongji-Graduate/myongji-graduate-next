f9affa455ef79fb8bb8a08a22b3dcbf515fafafa
xxxjinn <tlsguswls319@naver.com>
2025-09-17 02:22:53 +0900

    feat: 총 학점을 보여주는 함수 구현 및 적용

--- app/(sub-page)/timetable/components/create-timetable/create-timetable.tsx
+++ app/(sub-page)/timetable/components/create-timetable/create-timetable.tsx
@@ -4,16 +4,19 @@ import TitleBox from '@/app/ui/view/molecule/title-box/title-box';
 import ControlButtonGroup from './control-button-group';
 import { TimeTable } from '@/app/ui/view/molecule/time-table';
 import { useTimetableLecture } from '@/app/business/hooks/use-timetable-lecture.hook';
+import { calculateCurrentCredit } from '@/app/utils/timetable/timetable.util';
 
 function CreateTimetable() {
   const { lectures, removeLecture } = useTimetableLecture();
+  const totalCredit = calculateCurrentCredit(lectures);
 
   return (
-    <div className="flex flex-col gap-6 md:gap-8 pb-4 md:pb-6">
+    <div className="flex flex-col gap-6 pb-4 md:pb-6">
       <TitleBox title="시간표 생성">
         <p>미이수 과목들로 시간표를 만들고 관리해보세요!</p>
       </TitleBox>
       <ControlButtonGroup />
+      <p className="text-gray-400">총 학점: {totalCredit} 학점</p>
       <TimeTable data={lectures} onRemove={removeLecture} />
     </div>
   );
