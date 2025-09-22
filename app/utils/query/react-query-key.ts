ddd227b05c355c6f12a8fb06b881c4b7b65bce4c
xxxjinn <tlsguswls319@naver.com>
2025-09-22 19:25:47 +0900

    feat: 과목 fetch query 구현

--- app/utils/query/react-query-key.ts
+++ app/utils/query/react-query-key.ts
@@ -1,5 +1,6 @@
 export const QUERY_KEY = {
   SEARCH_LECTURE: 'search-lecture',
+  SEARCH_TIMETABLE_LECTURE: 'search-timetable-lecture',
   CREDIT: 'credit',
   USER: 'user',
   CATEGORY: 'category',
