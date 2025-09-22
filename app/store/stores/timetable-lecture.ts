97d578bab7ac1379de380ef560125b69dedc45f4
xxxjinn <tlsguswls319@naver.com>
2025-09-22 19:27:30 +0900

    feat: 과목 필터링 atom 생성

--- app/store/stores/timetable-lecture.ts
+++ app/store/stores/timetable-lecture.ts
@@ -4,3 +4,21 @@ import { atom } from 'jotai';
 /** 시간표 과목 store */
 
 export const timeTableLectureAtom = atom<TimetableLectureRow[]>([]);
+
+/** 과목 필터링 */
+
+interface timetableLectureFilter {
+  campus: string;
+  filter: string;
+  keyword: string;
+  professor: string;
+  recommendedCategory?: string;
+}
+
+export const timetableLectureFilterAtom = atom<timetableLectureFilter>({
+  campus: '인문',
+  filter: 'NOT_TAKEN',
+  keyword: '',
+  professor: '',
+  recommendedCategory: '',
+});
