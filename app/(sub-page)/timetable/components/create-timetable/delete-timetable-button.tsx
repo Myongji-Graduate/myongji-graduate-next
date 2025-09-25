be3f0925c100aeea33020ca03d1bfd080b00547a
xxxjinn <tlsguswls319@naver.com>
2025-09-25 19:07:30 +0900

    feat: 시간표 삭제 버튼 컴포넌트 구현

---
+++ app/(sub-page)/timetable/components/create-timetable/delete-timetable-button.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { useTimetableLecture } from '@/app/business/hooks/use-timetable-lecture.hook';
+import { useDeleteTimetable } from '@/app/store/querys/timetable/timetable';
+import Button from '@/app/ui/view/atom/button/button';
+import {
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogTitle,
+  AlertDialogCancel,
+} from '@/app/ui/view/molecule/alert-dialog/alert-dialog';
+import { toast } from '@/app/ui/view/molecule/toast/use-toast';
+import { useState } from 'react';
+
+function DeleteTimetableButton() {
+  const { mutate: deleteTimetable } = useDeleteTimetable();
+  const [open, setOpen] = useState(false);
+
+  const handleConfirmButton = () => {
+    deleteTimetable(undefined, {
+      onSuccess: (data) => {
+        toast({ title: '시간표를 삭제했습니다.' });
+      },
+      onError: (error) => {
+        toast({ title: '시간표 삭제에 실패했습니다.', variant: 'destructive' });
+        console.error(error);
+      },
+    });
+    setOpen(false);
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button label="삭제" variant="outlined" size="xs" />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>현재 저장된 시간표를 삭제할까요?</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter className="flex gap-6">
+          <AlertDialogCancel>취소</AlertDialogCancel>
+          <Button
+            label="확인"
+            className="text-primary font-bold bg-white hover:bg-white"
+            onClick={handleConfirmButton}
+          />
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export default DeleteTimetableButton;
