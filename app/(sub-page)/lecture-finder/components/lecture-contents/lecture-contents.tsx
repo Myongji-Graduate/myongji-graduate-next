0e8d1ebc80429bb7feef9e3d4839fd2dacfd61de
심유진 <syj11243@naver.com>
2025-10-19 19:05:12 +0900

    fix: 로딩 스피너 스타일 수정

--- app/(sub-page)/lecture-finder/components/lecture-contents/lecture-contents.tsx
+++ app/(sub-page)/lecture-finder/components/lecture-contents/lecture-contents.tsx
@@ -55,7 +55,14 @@ export default function LectureContents() {
         )}
 
         <div ref={sentinelRef} className="h-10" />
-        {isFetchingNext && <LoadingSpinner />}
+        {isFetchingNext && (
+          <div className="rounded-xl border-[1px] border-gray-300 w-full h-72 overflow-auto flex justify-center items-center">
+            <LoadingSpinner
+              className={'animate-spin shrink-0 h-12 w-12 mr-1.5 -ml-1 fill-gray-400'}
+              style={{ transition: `width 150ms` }}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
