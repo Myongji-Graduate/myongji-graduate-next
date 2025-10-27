022d895049903d6d64deffe4f8b1c01f3def1215
xxxjinn <tlsguswls319@naver.com>
2025-10-27 23:15:11 +0900

    refactor: interface 변수명 수정

--- app/business/services/timetable/timetable.command.ts
+++ app/business/services/timetable/timetable.command.ts
@@ -3,16 +3,16 @@
 import { API_PATH } from '../../api-path';
 import { instance } from '@/app/utils/api/instance';
 
-export interface timetableQuery {
+export interface TimetableQuery {
   year: number;
   semester: number;
 }
 
-export interface uploadTimetableQuery extends timetableQuery {
+export interface UploadTimetableQuery extends TimetableQuery {
   lecturesIds: number[];
 }
 
-export const uploadTimetable = async ({ year, semester, lecturesIds }: uploadTimetableQuery) => {
+export const uploadTimetable = async ({ year, semester, lecturesIds }: UploadTimetableQuery) => {
   await instance.post(`${API_PATH.timetable}/my/replace`, {
     year,
     semester,
@@ -20,11 +20,11 @@ export const uploadTimetable = async ({ year, semester, lecturesIds }: uploadTim
   });
 };
 
-export const fetchTimetable = async ({ year, semester }: timetableQuery) => {
+export const fetchTimetable = async ({ year, semester }: TimetableQuery) => {
   const response = await instance.get(`${API_PATH.timetable}/my?year=${year}&semester=${semester}`, {});
   return response.data;
 };
 
-export const deleteTimetable = async ({ year, semester }: timetableQuery) => {
+export const deleteTimetable = async ({ year, semester }: TimetableQuery) => {
   await instance.post(`${API_PATH.timetable}/my/delete`, { year, semester });
 };
