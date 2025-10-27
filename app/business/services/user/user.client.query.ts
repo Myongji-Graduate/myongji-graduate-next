855c547ef5b479ecef06f84dff58cfa35253a42b
xxxjinn <tlsguswls319@naver.com>
2025-10-27 23:00:52 +0900

    refactor: fetchUser query 분리

---
+++ app/business/services/user/user.client.query.ts
@@ -0,0 +1,13 @@
+'use client';
+
+import { QUERY_KEY } from '@/app/utils/query/react-query-key';
+import { useQuery } from '@tanstack/react-query';
+import { fetchUser } from './user.query';
+import { InitUserInfoResponse, UserInfoResponse } from './user.type';
+
+export const useFetchUser = () => {
+  return useQuery<InitUserInfoResponse | UserInfoResponse>({
+    queryKey: [QUERY_KEY.USER],
+    queryFn: fetchUser,
+  });
+};
