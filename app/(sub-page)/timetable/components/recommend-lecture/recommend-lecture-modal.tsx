e7754bb888f3b2ced85bf676b654f9e29d7de113
xxxjinn <tlsguswls319@naver.com>
2025-11-03 21:24:32 +0900

    style: list만 스크롤되도록

--- app/(sub-page)/timetable/components/recommend-lecture/recommend-lecture-modal.tsx
+++ app/(sub-page)/timetable/components/recommend-lecture/recommend-lecture-modal.tsx
@@ -43,21 +43,32 @@ async function RecommendLectureModal() {
 
   const ModalContent = () => {
     return (
-      <>
-        <TitleBox title="학기별 시간표 과목 추천">
-          <p>아직 듣지 않은 과목으로 남은 학기별 시간표를 자동으로 추천해드려요.</p>
-        </TitleBox>
-        <p className="text-lg md:text-xl text-center font-bold text-gray-500">
-          {user.studentName}님, 앞으로 {recommendLectureData.semestersLeft}학기 남았습니다!
-        </p>
-        {recommendLectureData.semestersLeft === 0 ? (
-          <div className="flex flex-col items-center justify-center">
-            <Image src="/assets/graduate-maru.png" alt="학사모 마루 이미지" className="mb-4" width={250} height={250} />
-          </div>
-        ) : (
-          <RecommendLectureContainer semesters={recommendLectureData.semesters} />
-        )}
-      </>
+      <div className="flex flex-col h-full overflow-hidden">
+        <div className="flex-shrink-0 p-4">
+          <TitleBox title="학기별 시간표 과목 추천">
+            <p>아직 듣지 않은 과목으로 남은 학기별 시간표를 자동으로 추천해드려요.</p>
+          </TitleBox>
+          <p className="text-lg md:text-xl text-center font-bold text-gray-500">
+            {user.studentName}님, 앞으로 {recommendLectureData.semestersLeft}학기 남았습니다!
+          </p>
+        </div>
+
+        <div className="flex-1 overflow-y-auto p-4">
+          {recommendLectureData.semestersLeft === 0 ? (
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                src="/assets/graduate-maru.png"
+                alt="학사모 마루 이미지"
+                className="mb-4"
+                width={250}
+                height={250}
+              />
+            </div>
+          ) : (
+            <RecommendLectureContainer semesters={recommendLectureData.semesters} />
+          )}
+        </div>
+      </div>
     );
   };
 
@@ -65,14 +76,14 @@ async function RecommendLectureModal() {
     <>
       <Responsive minWidth={570}>
         <Modal modalKey={DIALOG_KEY.RECOMMEND_LECTURE}>
-          <div className="flex flex-col gap-5 md:gap-6 pb-4 md:pb-6 w-[800px] max-lg:w-[450px]">
+          <div className="flex flex-col gap-5 md:gap-6 pb-4 md:pb-6 w-[800px] max-lg:w-[450px] h-[60vh] lg:h-[80vh]">
             <ModalContent />
           </div>
         </Modal>
       </Responsive>
       <Responsive maxWidth={569}>
         <Modal modalKey={DIALOG_KEY.RECOMMEND_LECTURE}>
-          <div className="flex flex-col gap-5 md:gap-6 pb-4 md:pb-6 w-[70vw]">
+          <div className="flex flex-col gap-5 md:gap-6 pb-4 md:pb-6 w-[70vw] h-[60vh]">
             <ModalContent />
           </div>
         </Modal>
