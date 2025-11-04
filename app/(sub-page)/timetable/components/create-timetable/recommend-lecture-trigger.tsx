fa0a082c286efcc4334b7e43ddd40e6881924e96
xxxjinn <tlsguswls319@naver.com>
2025-11-04 21:02:20 +0900

    refactor: 모달 open 간단하게 수정

--- app/(sub-page)/timetable/components/create-timetable/recommend-lecture-trigger.tsx
+++ app/(sub-page)/timetable/components/create-timetable/recommend-lecture-trigger.tsx
@@ -3,21 +3,11 @@ import Button from '@/app/ui/view/atom/button/button';
 import { DIALOG_KEY } from '@/app/utils/key/dialog-key.util';
 
 function RecommendLectureTrigger() {
-  const { open: recommendLectureDialogOpen } = useDialog(DIALOG_KEY.RECOMMEND_LECTURE);
-
-  const handleModalToggle = () => {
-    recommendLectureDialogOpen();
-  };
+  const { open } = useDialog(DIALOG_KEY.RECOMMEND_LECTURE);
 
   return (
     <div className="flex justify-start">
-      <Button
-        label="시간표 추천받기"
-        variant="outlined"
-        onClick={handleModalToggle}
-        size="xs"
-        className="h-[42px] w-[135px]"
-      />
+      <Button label="시간표 추천받기" variant="outlined" onClick={open} size="xs" className="h-[42px] w-[135px]" />
     </div>
   );
 }
