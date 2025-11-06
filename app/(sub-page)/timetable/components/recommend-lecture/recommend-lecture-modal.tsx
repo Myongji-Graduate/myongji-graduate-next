e595767b9aa601e81fd5024c59b1862de46d2795
xxxjinn <tlsguswls319@naver.com>
2025-11-06 21:23:12 +0900

    style: 모바일의 경우 문구 가운데 정렬

--- app/(sub-page)/timetable/components/recommend-lecture/recommend-lecture-modal.tsx
+++ app/(sub-page)/timetable/components/recommend-lecture/recommend-lecture-modal.tsx
@@ -45,7 +45,16 @@ async function RecommendLectureModal() {
       <div className="flex flex-col h-full overflow-hidden">
         <div className="flex-shrink-0 p-4 flex flex-col gap-4">
           <TitleBox title="학기별 시간표 과목 추천">
-            <p>아직 듣지 않은 과목으로 남은 학기별 시간표를 자동으로 추천해드려요.</p>
+            <Responsive minWidth={561}>
+              <p>아직 듣지 않은 과목으로 남은 학기별 시간표를 자동으로 추천해드려요.</p>
+            </Responsive>
+            <Responsive maxWidth={560}>
+              <p className="text-center leading-relaxed break-keep">
+                아직 듣지 않은 과목으로
+                <br />
+                남은 학기별 시간표를 자동으로 추천해드려요.
+              </p>
+            </Responsive>
           </TitleBox>
           <Suspense fallback={<UserCreditResultSkeleton />}>
             <UserCreditResult semestersLeft={recommendLectureData.semestersLeft} />
