664fd4d8acc7f8af46d6bcd6247ca45d31a03b8c
xxxjinn <tlsguswls319@naver.com>
2025-11-08 21:40:51 +0900

    refactor: timetableContent 컴포넌트 분리

--- app/(sub-page)/timetable/components/create-timetable/create-timetable.tsx
+++ app/(sub-page)/timetable/components/create-timetable/create-timetable.tsx
@@ -1,36 +1,10 @@
 'use client';
 
 import TitleBox from '@/app/ui/view/molecule/title-box/title-box';
-import { TimeTable } from '@/app/ui/view/molecule/time-table';
 import { useTimetableLecture } from '@/app/business/hooks/use-timetable-lecture.hook';
 import SearchModal from '@/app/ui/timetable/create-timetable/lecture/search-modal';
-import UnscheduledLectureList from './lecture/unscheduled-lecture-list';
-import { Suspense, useEffect } from 'react';
-import LoadingSpinner from '@/app/ui/view/atom/loading-spinner/loading-spinner';
-import { useAtom } from 'jotai';
-import { timeTableHydratedAtom } from '@/app/store/stores/timetable-lecture';
-import { useFetchTimetable } from '@/app/business/services/timetable/timetable.query';
 import TimetableButtonGroup from './timetable-button-group';
-
-function TimetableContent() {
-  const { lectures, removeLecture, initializeLectures, unscheduledLectures } = useTimetableLecture();
-  const { data: fetchLectures } = useFetchTimetable();
-  const [isHydrated, setIsHydrated] = useAtom(timeTableHydratedAtom);
-
-  useEffect(() => {
-    if (!isHydrated && fetchLectures.length > 0) {
-      initializeLectures(fetchLectures);
-      setIsHydrated(true);
-    }
-  }, [fetchLectures, initializeLectures, isHydrated, setIsHydrated]);
-
-  return (
-    <>
-      <TimeTable data={lectures} onRemove={removeLecture} />
-      {unscheduledLectures.length > 0 && <UnscheduledLectureList data={unscheduledLectures} />}
-    </>
-  );
-}
+import TimetableContent from './timetable-content';
 
 function CreateTimetable() {
   const { totalCredit } = useTimetableLecture();
@@ -42,18 +16,7 @@ function CreateTimetable() {
       </TitleBox>
       <TimetableButtonGroup />
       <p className="text-gray-400">총 학점: {totalCredit} 학점</p>
-      <Suspense
-        fallback={
-          <div className="rounded-xl w-full h-72 overflow-auto flex justify-center items-center">
-            <LoadingSpinner
-              className={'animate-spin shrink-0 h-12 w-12 mr-1.5 -ml-1 fill-gray-400'}
-              style={{ transition: `width 150ms` }}
-            />
-          </div>
-        }
-      >
-        <TimetableContent />
-      </Suspense>
+      <TimetableContent />
       <SearchModal />
     </div>
   );
