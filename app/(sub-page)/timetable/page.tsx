9b2d12561fc7bd4854a7d35acc0570fb51c7fba0
xxxjinn <tlsguswls319@naver.com>
2025-11-08 22:53:02 +0900

    refactor: 데이터 prefetch로 받아오도록 수정

--- app/(sub-page)/timetable/page.tsx
+++ app/(sub-page)/timetable/page.tsx
@@ -8,6 +8,8 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import { fetchUser } from '@/app/business/services/user/user.query';
 import { QUERY_KEY } from '@/app/utils/query/react-query-key';
 import RecommendLectureModal from './components/recommend-lecture/recommend-lecture-modal';
+import { fetchTimetable } from '@/app/business/services/timetable/timetable.command';
+import { CURRENT_YEAR, CURRENT_SEMESTER } from '@/app/utils/timetable/constants';
 
 export const metadata: Metadata = {
   title: '시간표',
@@ -16,14 +18,21 @@ export const metadata: Metadata = {
 
 async function TimetablePage() {
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({ queryKey: [QUERY_KEY.USER], queryFn: fetchUser });
+
+  await Promise.all([
+    queryClient.prefetchQuery({ queryKey: [QUERY_KEY.USER], queryFn: fetchUser }),
+    queryClient.prefetchQuery({
+      queryKey: [QUERY_KEY.TIMETABLE],
+      queryFn: () => fetchTimetable({ year: CURRENT_YEAR, semester: CURRENT_SEMESTER }),
+    }),
+  ]);
 
   return (
     <>
-      <ContentContainer className="flex flex-col gap-2 py-10 px-7 md:gap-6">
-        <CreateTimetable />
-      </ContentContainer>
       <HydrationBoundary state={dehydrate(queryClient)}>
+        <ContentContainer className="flex flex-col gap-2 py-10 px-7 md:gap-6">
+          <CreateTimetable />
+        </ContentContainer>
         <Drawer drawerKey={DIALOG_KEY.TIMETABLE_LECTURE_SEARCH}>
           <TimetableLectureSearch />
         </Drawer>
