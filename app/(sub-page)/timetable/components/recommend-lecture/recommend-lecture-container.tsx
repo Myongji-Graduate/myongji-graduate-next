7d6cabd9a3c6aa796ebe6976bd4067e5ccd8632a
xxxjinn <tlsguswls319@naver.com>
2025-11-12 22:21:01 +0900

    style: 추천과목 list 스타일 수정

--- app/(sub-page)/timetable/components/recommend-lecture/recommend-lecture-container.tsx
+++ app/(sub-page)/timetable/components/recommend-lecture/recommend-lecture-container.tsx
@@ -3,11 +3,11 @@ import RecommendLectureList from './recommend-lecture-list';
 
 function RecommendLectureContainer({ semesters }: { semesters: Semester[] }) {
   return (
-    <div className="py-2">
+    <div className="py-2 flex flex-col gap-6">
       {semesters.map((semester, index) => (
         <div key={semester.label} className={`${index === semesters.length - 1 ? '' : 'mb-6'} flex flex-col gap-3`}>
-          <p className="font-bold text-base md:text-lg">
-            {semester.label} [총 {semester.creditTarget}학점]
+          <p className="font-bold text-base md:text-lg text-gray-600">
+            {semester.label}학기 [총 {semester.creditTarget}학점]
           </p>
           <RecommendLectureList lectures={semester.lectures} />
         </div>
