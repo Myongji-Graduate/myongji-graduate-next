f154bb4c03aff7a44cf674e0ac8fa2aa3fb841d3
xxxjinn <tlsguswls319@naver.com>
2025-11-12 21:41:02 +0900

    feat: api 연결 함수 및 useQuery 작성

--- app/business/services/timetable/recommend-lecture.command.ts
+++ app/business/services/timetable/recommend-lecture.command.ts
@@ -1,8 +1,14 @@
-import { instance } from '@/app/utils/api/instance';
 import { API_PATH } from '../../api-path';
 import { RecommendLectureData } from './recommend-lecture.type';
+import { getToken } from '@/app/business/services/auth';
+import fetchAX from 'fetch-ax';
 
 export const fetchRecommendLecture = async () => {
-  const response = await instance.get<RecommendLectureData>(API_PATH.recommendLecture);
+  const token = await getToken();
+  const response = await fetchAX.get<RecommendLectureData>(API_PATH.recommendLecture, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   return response.data;
 };
