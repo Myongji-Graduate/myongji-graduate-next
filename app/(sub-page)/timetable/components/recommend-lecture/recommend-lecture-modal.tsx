60fcec888772b5d7164ba24baef8e0111ba4a4c5
xxxjinn <tlsguswls319@naver.com>
2025-11-12 21:41:17 +0900

    refactor: 모달 수정

--- app/(sub-page)/timetable/components/recommend-lecture/recommend-lecture-modal.tsx
+++ app/(sub-page)/timetable/components/recommend-lecture/recommend-lecture-modal.tsx
@@ -1,29 +1,60 @@
+'use client';
+
 import Image from 'next/image';
 import RecommendLectureContainer from './recommend-lecture-container';
 import TitleBox from '@/app/ui/view/molecule/title-box/title-box';
 import Modal from '@/app/ui/view/molecule/modal/modal';
 import { DIALOG_KEY } from '@/app/utils/key/dialog-key.util';
 import Responsive from '@/app/ui/responsive';
-import UserCreditResult from '@/app/ui/user/user-credit-result/user-credit-result';
 import UserCreditResultSkeleton from '@/app/ui/user/user-credit-result/user-credit-result-skeleton';
-import { Suspense } from 'react';
-import { fetchRecommendLecture } from '@/app/business/services/timetable/recommend-lecture.command';
+import { useFetchRecommendLecture } from '@/app/business/services/timetable/recommend-lecture.query';
+import useDialog from '@/app/hooks/useDialog';
+import Skeleton from '@/app/utils/skeleton';
+import dynamic from 'next/dynamic';
 
-async function RecommendLectureModal() {
-  const recommendLectureData = await fetchRecommendLecture();
+function RecommendLectureModal() {
+  const { isOpen } = useDialog(DIALOG_KEY.RECOMMEND_LECTURE);
+  const { data: recommendLectureData, isLoading } = useFetchRecommendLecture(isOpen);
 
-  const normalizedRecommendLectureData = {
-    ...recommendLectureData,
-    semesters: recommendLectureData.semesters.map((semester) => ({
-      ...semester,
-      lectures: semester.lectures.map((lecture) => ({
-        code: lecture.id,
-        ...lecture,
-      })),
-    })),
-  };
+  const UserCreditResult = dynamic(() => import('@/app/ui/user/user-credit-result/user-credit-result'), { ssr: false });
 
   const ModalContent = () => {
+    if (isLoading || !recommendLectureData) {
+      return (
+        <div className="flex flex-col h-full overflow-hidden">
+          <div className="flex-shrink-0 p-4 flex flex-col gap-4">
+            <TitleBox title="학기별 시간표 과목 추천">
+              <Responsive minWidth={561}>
+                <p>아직 듣지 않은 과목으로 남은 학기별 시간표를 자동으로 추천해드려요.</p>
+              </Responsive>
+              <Responsive maxWidth={560}>
+                <p className="text-center leading-relaxed break-keep">
+                  아직 듣지 않은 과목으로
+                  <br />
+                  남은 학기별 시간표를 자동으로 추천해드려요.
+                </p>
+              </Responsive>
+            </TitleBox>
+            <UserCreditResultSkeleton />
+          </div>
+          <div className="flex-1 overflow-y-auto p-4">
+            <Skeleton className="h-full" />
+          </div>
+        </div>
+      );
+    }
+
+    const normalizedRecommendLectureData = {
+      ...recommendLectureData,
+      semesters: recommendLectureData.semesters.map((semester) => ({
+        ...semester,
+        lectures: semester.lectures.map((lecture) => ({
+          code: lecture.id,
+          ...lecture,
+        })),
+      })),
+    };
+
     return (
       <div className="flex flex-col h-full overflow-hidden">
         <div className="flex-shrink-0 p-4 flex flex-col gap-4">
@@ -39,9 +70,7 @@ async function RecommendLectureModal() {
               </p>
             </Responsive>
           </TitleBox>
-          <Suspense fallback={<UserCreditResultSkeleton />}>
-            <UserCreditResult semestersLeft={normalizedRecommendLectureData.semestersLeft} />
-          </Suspense>
+          <UserCreditResult semestersLeft={normalizedRecommendLectureData.semestersLeft} />
         </div>
         <div className="flex-1 overflow-y-auto p-4">
           {normalizedRecommendLectureData.semestersLeft === 0 ? (
