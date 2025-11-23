aea0d1ae4b2ae8734df81ebf4ea1057bc2eb9410
xxxjinn <tlsguswls319@naver.com>
2025-11-23 22:48:53 +0900

    refactor: 불필요한 export 삭제

--- app/business/services/timetable/constants.ts
+++ app/business/services/timetable/constants.ts
@@ -1,9 +1,9 @@
-export function getCurrentYear() {
+function getCurrentYear() {
   const now = new Date();
   return now.getFullYear();
 }
 
-export function getCurrentSemester() {
+function getCurrentSemester() {
   const now = new Date();
   const month = now.getMonth() + 1;
 
