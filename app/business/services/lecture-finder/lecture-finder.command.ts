3d692ed9e5e3eecb67207105c401d80fd0e7332f
심유진 <syj11243@naver.com>
2025-12-03 16:07:03 +0900

    feat: res.status === 400 error.messege 추가

--- app/business/services/lecture-finder/lecture-finder.command.ts
+++ app/business/services/lecture-finder/lecture-finder.command.ts
@@ -69,6 +69,10 @@ export async function fetchPopularByCategoryPaged(query: PopularByCategoryQuery 
     throw new SearchError('해당 카테고리에 해당하는 데이터가 존재하지 않습니다.', res.status);
   }
 
+  if (res.status === 400) {
+    throw new SearchError('해당 학과 학번에 해당하는 데이터가 존재하지 않습니다.', res.status);
+  }
+
   if (!res.ok) {
     throw new SearchError('강의 검색 중 오류가 발생했습니다.', res.status);
   }
@@ -92,6 +96,10 @@ export async function fetchPopularAllPaged(
 
   if (res.status === 404) {
     throw new SearchError('해당 카테고리에 해당하는 데이터가 존재하지 않습니다.', res.status);
+  }
+
+  if (res.status === 400) {
+    throw new SearchError('해당 학과 학번에 해당하는 데이터가 존재하지 않습니다.', res.status);
   }
 
   if (!res.ok) {
