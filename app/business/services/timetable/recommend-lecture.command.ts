41160a505d57760e10c0c8589aa39f081528ce17
xxxjinn <tlsguswls319@naver.com>
2025-12-16 20:31:27 +0900

    feat: 과목추천 api 에러처리 추가

--- app/business/services/timetable/recommend-lecture.command.ts
+++ app/business/services/timetable/recommend-lecture.command.ts
@@ -3,14 +3,22 @@
 import { API_PATH } from '../../api-path';
 import { RecommendLectureData } from './recommend-lecture.type';
 import { getToken } from '@/app/business/services/auth';
-import fetchAX from 'fetch-ax';
+import fetchAX, { FetchAxError } from 'fetch-ax';
 
 export const fetchRecommendLecture = async () => {
   const token = await getToken();
-  const response = await fetchAX.get<RecommendLectureData>(API_PATH.recommendLecture, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return response.data;
+  try {
+    const response = await fetchAX.get<RecommendLectureData>(API_PATH.recommendLecture, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return response.data;
+  } catch (err: unknown) {
+    if (err instanceof FetchAxError) {
+      if (err.response?.status === 400) {
+        throw new Error('성적표를 재등록해주세요');
+      }
+    }
+
+    throw new Error('채널톡으로 문의해주세요');
+  }
 };
