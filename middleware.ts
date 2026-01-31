f5a2c042ef048ed1c8203754c64a1cfd85187dd5
심유진 <syj11243@naver.com>
2026-01-31 21:09:35 +0900

    remove: 점검중페이지 제거

--- middleware.ts
+++ middleware.ts
@@ -38,8 +38,6 @@ const allowedGuestPath = [
 
 const allowInitUserPath = ['/', '/tutorial', '/grade-upload', '/anonymous', '/anonymous/result', '/lecture-finder'];
 
-const MAINTENANCE_MODE = true;
-
 function isAllowedGuestPath(path: string, strict: boolean = false) {
   const allowedPath = strict ? allowedOnlyGuestPath : allowedGuestPath;
 
@@ -47,16 +45,6 @@ function isAllowedGuestPath(path: string, strict: boolean = false) {
 }
 
 export async function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-
-  if (MAINTENANCE_MODE && pathname !== '/maintenance') {
-    return Response.redirect(new URL('/maintenance', request.url));
-  }
-
-  if (MAINTENANCE_MODE) {
-    return;
-  }
-
   const auth = await getAuth(request);
 
   if (auth.role === 'expired') {
